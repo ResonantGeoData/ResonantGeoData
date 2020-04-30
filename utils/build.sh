5acfb395e83a20c7d6730c0744e3e73a377f4369
David Manthey <david.manthey@kitware.com>
2020-04-30 10:24:34 -0400

    Add a simple scoring algorithm for testing.

--- utils/build.sh
+++ utils/build.sh
@@ -2,3 +2,4 @@
 docker build --force-rm -t viame/detector -f Dockerfile-viame-detector .
 docker build --force-rm -t viame/scorer -f Dockerfile-viame-scorer .
 docker build --force-rm -t socom/catalgo -f Dockerfile-cat-algorithm .
+docker build --force-rm -t socom/binscorer -f Dockerfile-binary-scorer .
