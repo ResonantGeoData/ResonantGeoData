c72371a942d306f556630553080b94c89a23ed87
banesullivan <banesulli@gmail.com>
2020-05-05 11:10:21 -0400

    Fix task detail URL

--- core/urls.py
+++ core/urls.py
@@ -12,7 +12,11 @@ urlpatterns = [
     path('jobs/new/', views.JobCreateView.as_view(), name='new-job'),
     path('jobs/<str:creator>/<int:pk>/', views.JobDetailView.as_view(), name='job-detail'),
     path('tasks/', views.tasks, name='tasks'),
+<<<<<<< HEAD
     path('task/<int:pk>-<str:name>/', views.TaskDetailView.as_view(), name='task-detail'),
+=======
+    path('task/<int:pk>-<str:name>/', views.TaskDetailView.as_view(), name="task-detail"),
+>>>>>>> Fix task detail URL
 ]
 
 handler500 = views.handler500
