f9bb1f4d6459715b211fa8f9f1c266ab6bf8b6fc
Brian Helba <brian.helba@kitware.com>
2020-05-20 13:43:03 -0400

    Add Terraform deployment configuration

---
+++ terraform/sts.tf
@@ -0,0 +1,44 @@
+data "aws_iam_user" "heroku_user" {
+  user_name = "rgd-heroku"
+}
+
+data "aws_s3_bucket" "storage" {
+  bucket = "resonantgeodata-files"
+}
+
+resource "aws_iam_role" "storage_upload" {
+  name                 = "rgd-storage-upload-sts"
+  max_session_duration = 12 * 60 * 60 # 12 hours
+  assume_role_policy   = data.aws_iam_policy_document.storage_upload_assumeRolePolicy.json
+}
+
+data "aws_iam_policy_document" "storage_upload_assumeRolePolicy" {
+  statement {
+    principals {
+      type = "AWS"
+      identifiers = [
+        data.aws_iam_user.heroku_user.arn,
+      ]
+    }
+    actions = [
+      "sts:AssumeRole",
+    ]
+  }
+}
+
+resource "aws_iam_role_policy" "storage_upload" {
+  name   = "rgd-storage-upload-sts"
+  role   = aws_iam_role.storage_upload.name
+  policy = data.aws_iam_policy_document.storage_upload.json
+}
+
+data "aws_iam_policy_document" "storage_upload" {
+  statement {
+    actions = [
+      "s3:PutObject",
+    ]
+    resources = [
+      "${data.aws_s3_bucket.storage.arn}/*",
+    ]
+  }
+}
