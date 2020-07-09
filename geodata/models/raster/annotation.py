52a41d88577f81fb55b236164bf436e7f73a45b7
banesullivan <banesulli@gmail.com>
2020-07-09 14:33:57 -0400

    Fix flake8 issues

--- geodata/models/raster/annotation.py
+++ geodata/models/raster/annotation.py
@@ -1,9 +1,4 @@
 from django.contrib.gis.db import models
-from django.contrib.postgres import fields
-from django.db import transaction
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-from s3_file_field import S3FileField
 
 from .base import RasterEntry
 from ..common import ModifiableEntry
