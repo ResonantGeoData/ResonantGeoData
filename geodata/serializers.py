87b4a86c5638d95a7ef3e16a8f8efa12a6f71ad0
mattrix27 <mtung@mit.edu>
2020-08-24 00:27:40 -0400

    Add read_only_fields

--- geodata/serializers.py
+++ geodata/serializers.py
@@ -6,6 +6,20 @@ from rgd import utility
 from . import models
 
 
+class GeometryEntrySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.GeometryEntry
+        fields = '__all__'
+        read_only_fields = ['geometry_archive', 'spatialentry_ptr']
+
+
+class RasterEntrySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.RasterEntry
+        fields = '__all__'
+        read_only_fields = ['spatialentry_ptr']
+
+
 class SpatialEntrySerializer(serializers.ModelSerializer):
     def to_representation(self, value):
         ret = super().to_representation(value)
