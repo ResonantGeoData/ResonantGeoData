4f17256f21d5cac3288459a1fc92d949bfd6d644
banesullivan <bane.sullivan@kitware.com>
2020-09-15 12:34:05 -0600

    Imports

--- geodata/models/__init__.py
+++ geodata/models/__init__.py
@@ -1,5 +1,6 @@
 # These are models we want to expose in the top-level model namespace
 
 from .common import ArbitraryFile, ChecksumFile, ModifiableEntry, SpatialEntry  # noqa
+from .fmv import *  # noqa
 from .geometry import *  # noqa
 from .imagery import *  # noqa
---
+++ geodata/models/fmv/__init__.py
@@ -0,0 +1,1 @@
+from .base import FMVEntry, FMVFile  # noqa
