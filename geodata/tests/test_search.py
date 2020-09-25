a59b46e6532322ebfa963244d2d68d74d318a9a3
David Manthey <david.manthey@kitware.com>
2020-09-25 11:02:45 -0400

    Add test for some search functions.

---
+++ geodata/tests/test_search.py
@@ -0,0 +1,190 @@
+import datetime
+import json
+
+import pytest
+
+from . import factories
+from .datastore import datastore
+
+SampleFiles = [
+    {'name': '20091021202517-01000100-VIS_0001.ntf'},
+    {'name': 'aerial_rgba_000003.tiff'},
+    {'name': 'cclc_schu_100.tif'},
+    {'name': 'landcover_sample_2000.tif'},
+    {'name': 'paris_france_10.tiff'},
+    {'name': 'rgb_geotiff.tiff'},
+    {'name': 'RomanColosseum_WV2mulitband_10.tif'},
+]
+
+
+def _load_sample_files():
+    for testfile in SampleFiles:
+        imagefile = factories.ImageFileFactory(
+            file__filename=testfile['name'],
+            file__from_path=datastore.fetch(testfile['name']),
+        )
+        factories.RasterEntryFactory(
+            name=testfile['name'], images=[imagefile.baseimagefile_ptr.imageentry.id]
+        )
+
+
+@pytest.mark.django_db(transaction=True)
+def test_search_near_point(client):
+    _load_sample_files()
+    items = json.loads(client.get('/api/geodata/near_point').content)
+    assert len(items) == len(SampleFiles)
+    items = json.loads(
+        client.get('/api/geodata/near_point', {'longitude': -79, 'latitude': 43}).content
+    )
+    assert len(items) == 1
+    items = json.loads(
+        client.get(
+            '/api/geodata/near_point', {'longitude': -79, 'latitude': 43, 'radius': 1000000}
+        ).content
+    )
+    assert len(items) == 3
+    timestr = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    items = json.loads(
+        client.get(
+            '/api/geodata/raster/near_point',
+            {'time': timestr, 'timefield': 'acquisition', 'timespan': 86400},
+        ).content
+    )
+    assert len(items) == len(SampleFiles)
+    items = json.loads(
+        client.get(
+            '/api/geodata/raster/near_point',
+            {'time': timestr, 'timefield': 'acquisition', 'timespan': 0},
+        ).content
+    )
+    assert len(items) == 0
+
+
+@pytest.mark.django_db(transaction=True)
+def test_search_near_point_extent(client):
+    _load_sample_files()
+    results = json.loads(client.get('/api/geodata/near_point/extent').content)
+    assert results['count'] == len(SampleFiles)
+    results = json.loads(
+        client.get('/api/geodata/near_point/extent', {'longitude': -79, 'latitude': 43}).content
+    )
+    assert results['count'] == 1
+    results = json.loads(
+        client.get(
+            '/api/geodata/near_point/extent', {'longitude': -79, 'latitude': 43, 'radius': 1000000}
+        ).content
+    )
+    assert results['count'] == 3
+    timestr = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    results = json.loads(
+        client.get(
+            '/api/geodata/raster/near_point/extent',
+            {'time': timestr, 'timefield': 'acquisition', 'timespan': 86400},
+        ).content
+    )
+    assert results['count'] == len(SampleFiles)
+    results = json.loads(
+        client.get(
+            '/api/geodata/raster/near_point/extent',
+            {'time': timestr, 'timefield': 'acquisition', 'timespan': 0},
+        ).content
+    )
+    assert results['count'] == 0
+
+
+@pytest.mark.django_db(transaction=True)
+def test_search_bounding_box(client):
+    _load_sample_files()
+    items = json.loads(client.get('/api/geodata/bounding_box').content)
+    assert len(items) == len(SampleFiles)
+    items = json.loads(
+        client.get(
+            '/api/geodata/bounding_box',
+            {
+                'minimum_longitude': -80,
+                'maximum_longitude': -79,
+                'minimum_latitude': 43,
+                'maximum_latitude': 44,
+            },
+        ).content
+    )
+    assert len(items) == 1
+    items = json.loads(
+        client.get(
+            '/api/geodata/bounding_box',
+            {
+                'minimum_longitude': -90,
+                'maximum_longitude': -70,
+                'minimum_latitude': 30,
+                'maximum_latitude': 50,
+            },
+        ).content
+    )
+    assert len(items) == 3
+    timestr = (datetime.datetime.now() + datetime.timedelta(days=1)).strftime('%Y-%m-%d %H:%M:%S')
+    oldtimestr = (datetime.datetime.now() - datetime.timedelta(days=1)).strftime(
+        '%Y-%m-%d %H:%M:%S'
+    )
+    items = json.loads(
+        client.get(
+            '/api/geodata/raster/bounding_box',
+            {'start_time': oldtimestr, 'end_time': timestr, 'timefield': 'acquisition'},
+        ).content
+    )
+    assert len(items) == len(SampleFiles)
+    items = json.loads(
+        client.get(
+            '/api/geodata/raster/bounding_box',
+            {'start_time': timestr, 'end_time': timestr, 'timefield': 'acquisition'},
+        ).content
+    )
+    assert len(items) == 0
+
+
+@pytest.mark.django_db(transaction=True)
+def test_search_bounding_box_extent(client):
+    _load_sample_files()
+    results = json.loads(client.get('/api/geodata/bounding_box/extent').content)
+    assert results['count'] == len(SampleFiles)
+    results = json.loads(
+        client.get(
+            '/api/geodata/bounding_box/extent',
+            {
+                'minimum_longitude': -80,
+                'maximum_longitude': -79,
+                'minimum_latitude': 43,
+                'maximum_latitude': 44,
+            },
+        ).content
+    )
+    assert results['count'] == 1
+    results = json.loads(
+        client.get(
+            '/api/geodata/bounding_box/extent',
+            {
+                'minimum_longitude': -90,
+                'maximum_longitude': -70,
+                'minimum_latitude': 30,
+                'maximum_latitude': 50,
+            },
+        ).content
+    )
+    assert results['count'] == 3
+    timestr = (datetime.datetime.now() + datetime.timedelta(days=1)).strftime('%Y-%m-%d %H:%M:%S')
+    oldtimestr = (datetime.datetime.now() - datetime.timedelta(days=1)).strftime(
+        '%Y-%m-%d %H:%M:%S'
+    )
+    results = json.loads(
+        client.get(
+            '/api/geodata/raster/bounding_box/extent',
+            {'start_time': oldtimestr, 'end_time': timestr, 'timefield': 'acquisition'},
+        ).content
+    )
+    assert results['count'] == len(SampleFiles)
+    results = json.loads(
+        client.get(
+            '/api/geodata/raster/bounding_box/extent',
+            {'start_time': timestr, 'end_time': timestr, 'timefield': 'acquisition'},
+        ).content
+    )
+    assert results['count'] == 0
