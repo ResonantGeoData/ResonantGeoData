a031e9d6c22c17a9e87cc645d71fb42da79959bc
Bane Sullivan <banesullivan@gmail.com>
2020-10-06 09:08:25 -0600

    Testing improvements (#172)
    * Add memprof and durations to pytest
    
    * Speedup KLV test
    
    * Make search time queries timezone aware
    
    * Ignore RemovedInDjango40Warning in pytest for now
    
    * Move durations to [pytest] addopts

--- rgd/geodata/tests/test_fmv.py
+++ rgd/geodata/tests/test_fmv.py
@@ -9,8 +9,8 @@ from .datastore import datastore
 @pytest.mark.django_db(transaction=True)
 def test_populate_fmv_entry_from_klv_file():
     fmv_entry = factories.FMVEntryFactory(
-        klv_file__filename='09172008flight1tape3_2.mpg.klv',
-        klv_file__from_path=datastore.fetch('09172008flight1tape3_2.mpg.klv'),
+        klv_file__filename='subset_metadata.klv',
+        klv_file__from_path=datastore.fetch('subset_metadata.klv'),
     )
 
     assert _populate_fmv_entry(fmv_entry)
