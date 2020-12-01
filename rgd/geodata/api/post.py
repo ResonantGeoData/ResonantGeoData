063b3164abe406d9310e29cc2a007d22608801ef
banesullivan <bane.sullivan@kitware.com>
2020-12-01 14:40:53 -0700

    Prevent POST endpoint from creating duplicates

--- rgd/geodata/api/post.py
+++ rgd/geodata/api/post.py
@@ -1,3 +1,4 @@
+from django.core.exceptions import ObjectDoesNotExist
 from django.http import JsonResponse
 from drf_yasg2.utils import swagger_auto_schema
 from rest_framework.decorators import api_view
@@ -14,7 +15,11 @@ from ..models.imagery import ConvertedImageFile, ImageEntry
 def convert_image_to_cog(request, pk, format=None):
     # TODO: use format option if supporting things other than COG.
     img = ImageEntry.objects.get(id=pk)
-    c = ConvertedImageFile()
-    c.source_image = img
+    # Do not create duplicate conversions
+    try:
+        c = img.convertedimagefile
+    except ObjectDoesNotExist:
+        c = ConvertedImageFile()
+        c.source_image = img
     c.save()
     return JsonResponse({'converted_pk': c.pk})
