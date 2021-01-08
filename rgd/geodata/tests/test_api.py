2511728a6907d303c9e9325e55a6ba5cf64bab97
banesullivan <bane.sullivan@kitware.com>
2021-01-07 22:04:03 -0700

    Use lighterweight data for API tests

--- rgd/geodata/tests/test_api.py
+++ rgd/geodata/tests/test_api.py
@@ -14,8 +14,8 @@ def arbitrary_file():
 
 
 @pytest.fixture()
-def elevation_image():
-    name = 'Elevation.tif'
+def landsat_image():
+    name = 'LC08_L1TP_034032_20200429_20200509_01_T1_sr_band1.tif'
     imagefile = factories.ImageFileFactory(
         file__filename=name,
         file__from_path=datastore.fetch(name),
@@ -24,8 +24,18 @@ def elevation_image():
 
 
 @pytest.fixture()
-def elevation_raster():
-    name = 'Elevation.tif'
+def astro_image():
+    name = 'astro.png'
+    imagefile = factories.ImageFileFactory(
+        file__filename=name,
+        file__from_path=datastore.fetch(name),
+    )
+    return imagefile.baseimagefile_ptr.imageentry
+
+
+@pytest.fixture()
+def landsat_raster():
+    name = 'LC08_L1TP_034032_20200429_20200509_01_T1_sr_band1.tif'
     imagefile = factories.ImageFileFactory(
         file__filename=name,
         file__from_path=datastore.fetch(name),
@@ -57,9 +67,9 @@ def test_download_file(client, arbitrary_file):
 
 
 @pytest.mark.django_db(transaction=True)
-def test_get_status(client, elevation_image):
+def test_get_status(client, astro_image):
     model = 'ImageFile'
-    id = elevation_image.image_file.imagefile.id
+    id = astro_image.image_file.imagefile.id
     result = client.get(f'/api/geodata/status/{model}/{id}')
     assert result.status_code == 200
     with pytest.raises(AttributeError):
@@ -74,8 +84,8 @@ def test_download_arbitry_file(client, arbitrary_file):
 
 
 @pytest.mark.django_db(transaction=True)
-def test_download_image_entry_file(client, elevation_image):
-    pk = elevation_image.pk
+def test_download_image_entry_file(client, astro_image):
+    pk = astro_image.pk
     result = client.get(f'/api/geodata/imagery/image_entry/{pk}/data')
     assert result.status_code == 302 or result.status_code == 200
 
@@ -91,9 +101,9 @@ def test_get_arbitrary_file(client, arbitrary_file):
 
 
 @pytest.mark.django_db(transaction=True)
-def test_get_spatial_entry(client, elevation_raster):
+def test_get_spatial_entry(client, landsat_raster):
     """Test individual GET for SpatialEntry model."""
-    pk = elevation_raster.rastermetaentry.spatial_id
+    pk = landsat_raster.rastermetaentry.spatial_id
     content = json.loads(client.get(f'/api/geodata/common/spatial_entry/{pk}/').content)
     assert content
     assert content['footprint']
@@ -101,18 +111,18 @@ def test_get_spatial_entry(client, elevation_raster):
 
 
 @pytest.mark.django_db(transaction=True)
-def test_create_get_subsampled_image(client, elevation_image):
+def test_create_get_subsampled_image(client, astro_image):
     """Test POST and GET for SubsampledImage model."""
     client.post(
         '/api/geodata/imagery/subsample',
         {
-            'source_image': elevation_image.id,
+            'source_image': astro_image.id,
             'sample_type': 'pixel box',
             'sample_parameters': {'umax': 100, 'umin': 0, 'vmax': 200, 'vmin': 0},
         },
     )
     # TODO: wait for the task kicked of by POST to complete?
-    sub = models.imagery.SubsampledImage.objects.get(source_image=elevation_image.id)
+    sub = models.imagery.SubsampledImage.objects.get(source_image=landsat_image.id)
     assert sub.data
     # Test the GET
     content = json.loads(client.get(f'/api/geodata/imagery/subsample/{sub.id}/').content)
@@ -120,14 +130,14 @@ def test_create_get_subsampled_image(client, elevation_image):
 
 
 @pytest.mark.django_db(transaction=True)
-def test_create_and_download_cog(client, elevation_image):
+def test_create_and_download_cog(client, landsat_image):
     """Test POST for ConvertedImageFile model."""
     client.post(
         '/api/geodata/imagery/cog',
-        {'source_image': elevation_image.id},
+        {'source_image': landsat_image.id},
     )
     # Check that a COG was generated
-    cog = models.imagery.ConvertedImageFile.objects.get(source_image=elevation_image.id)
+    cog = models.imagery.ConvertedImageFile.objects.get(source_image=landsat_image.id)
     # NOTE: This doesn't actually verify the file is in COG format. Assumed.
     assert cog.converted_file
     # Also test download endpoint here:
