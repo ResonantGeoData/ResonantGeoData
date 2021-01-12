80ee8d8502ae659db77cfb356f1ce2e79f0289d2
banesullivan <bane.sullivan@kitware.com>
2021-01-12 09:25:39 -0700

    Prevent duplicate SubsampledImage models on POST

--- rgd/geodata/tests/test_api.py
+++ rgd/geodata/tests/test_api.py
@@ -115,13 +115,14 @@ def test_get_spatial_entry(client, landsat_raster):
 @pytest.mark.django_db(transaction=True)
 def test_create_get_subsampled_image(client, astro_image):
     """Test POST and GET for SubsampledImage model."""
+    payload = {
+        'source_image': astro_image.pk,
+        'sample_type': 'pixel box',
+        'sample_parameters': json.dumps({'umax': 100, 'umin': 0, 'vmax': 200, 'vmin': 0}),
+    }
     response = client.post(
         '/api/geodata/imagery/subsample',
-        {
-            'source_image': astro_image.pk,
-            'sample_type': 'pixel box',
-            'sample_parameters': json.dumps({'umax': 100, 'umin': 0, 'vmax': 200, 'vmin': 0}),
-        },
+        payload
     )
     assert response.status_code == 201, response.content
     content = json.loads(response.content)
@@ -131,7 +132,14 @@ def test_create_get_subsampled_image(client, astro_image):
     # Test the GET
     response = client.get(f'/api/geodata/imagery/subsample/{pk}')
     assert response.status_code == 200, response.content
-
+    # Now test to make sure the serializer prevents duplicates
+    response = client.post(
+        '/api/geodata/imagery/subsample',
+        payload
+    )
+    assert response.status_code == 201, response.content
+    content = json.loads(response.content)
+    assert pk == content['pk']  # Compare against original PK
 
 @pytest.mark.django_db(transaction=True)
 def test_create_and_download_cog(client, landsat_image):
