7e206c1e0c917ed8b66d4348625e7299afa07edf
banesullivan <bane.sullivan@kitware.com>
2021-01-19 10:12:45 -0700

    Add test to validate sorting

--- rgd/geodata/tests/test_filters.py
+++ rgd/geodata/tests/test_filters.py
@@ -20,7 +20,7 @@ def sample_raster_a():
         name='20091021202517-01000100-VIS_0001.ntf',
         image_set=image_set,
     )
-    return RasterMetaEntry.objects.filter(parent_raster=raster).get()
+    return RasterMetaEntry.objects.get(parent_raster=raster)
 
 
 @pytest.fixture
@@ -36,7 +36,31 @@ def sample_raster_b():
         name='cclc_schu_100.tif',
         image_set=image_set,
     )
-    return RasterMetaEntry.objects.filter(parent_raster=raster).get()
+    return RasterMetaEntry.objects.get(parent_raster=raster)
+
+
+@pytest.mark.django_db(transaction=True)
+def test_q_distance(sample_raster_a, sample_raster_b):
+    assert SpatialEntry.objects.count() == 2
+    # Make sure all are returned sorted by distance
+    filterset = SpatialEntryFilter(
+        data={
+            'q': f'SRID=4326;{sample_raster_a.footprint.wkt}',
+        }
+    )
+    assert filterset.is_valid()
+    qs = filterset.filter_queryset(SpatialEntry.objects.all())
+    assert qs.count() == 2
+    assert qs.first().spatial_id == sample_raster_a.spatial_id
+    filterset = SpatialEntryFilter(
+        data={
+            'q': f'SRID=4326;{sample_raster_b.footprint.wkt}',
+        }
+    )
+    assert filterset.is_valid()
+    qs = filterset.filter_queryset(SpatialEntry.objects.all())
+    assert qs.count() == 2
+    assert qs.first().spatial_id == sample_raster_b.spatial_id
 
 
 @pytest.mark.django_db(transaction=True)
