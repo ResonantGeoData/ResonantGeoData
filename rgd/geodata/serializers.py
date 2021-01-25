bfb566973817ecfe81f1b562f76167170a1a4ef3
Matt Covalt <mcovalt@mailbox.org>
2021-01-25 11:25:59 -0500

    Check permissions on serializer where POSTing

--- rgd/geodata/serializers.py
+++ rgd/geodata/serializers.py
@@ -4,6 +4,7 @@ from rest_framework import serializers
 from rest_framework.reverse import reverse
 
 from rgd import utility
+from rgd.geodata.permissions import check_write_perm
 
 from . import models
 
@@ -27,6 +28,10 @@ class GeometryEntrySerializer(SpatialEntrySerializer):
 
 
 class ConvertedImageFileSerializer(serializers.ModelSerializer):
+    def validate_source_image(self, value):
+        if 'request' in self.context:
+            check_write_perm(self.context['request'].user, value)
+
     class Meta:
         model = models.ConvertedImageFile
         fields = ['source_image', 'pk', 'status', 'failure_reason']
@@ -54,6 +59,10 @@ class SubsampledImageSerializer(serializers.ModelSerializer):
     data = serializers.HyperlinkedRelatedField(
         many=False, read_only=True, view_name='arbitrary-file-data'
     )
+
+    def validate_source_image(self, value):
+        if 'request' in self.context:
+            check_write_perm(self.context['request'].user, value)
 
     def to_representation(self, value):
         ret = super().to_representation(value)
