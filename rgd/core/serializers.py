664c8611ca5899f2b7efe6a65c57c36884b45687
banesullivan <bane.sullivan@kitware.com>
2021-01-26 12:30:29 -0700

    Remove core app

--- rgd/core/serializers.py
+++
@@ -1,5 +0,0 @@
-from rgd import utility
-
-from . import models
-
-utility.make_serializers(globals(), models)
--- rgd/urls.py
+++ rgd/urls.py
@@ -58,7 +58,6 @@ urlpatterns = [
     path('oauth/', include('oauth2_provider.urls', namespace='oauth2_provider')),
     path('admin/', admin.site.urls),
     path('api/s3-upload/', include('s3_file_field.urls')),
-    # path('', include('rgd.core.urls')),
     path('', include('rgd.geodata.urls')),
 ]
 
