a7bfa28b21253c6b1bf96eb0e3882f7838097c7f
banesullivan <bane.sullivan@kitware.com>
2021-01-26 12:24:21 -0700

    Merge branch 'master' into s3-support

--- rgd/core/urls.py
+++ rgd/core/urls.py
@@ -50,7 +50,6 @@ urlpatterns = [
     path('jobs/<str:creator>/<int:pk>/', views.JobDetailView.as_view(), name='job-detail'),
     path('tasks/', views.tasks, name='tasks'),
     path('task/<int:pk>-<str:name>/', views.TaskDetailView.as_view(), name='task-detail'),
-    path('api/core/download/<model>/<int:id>/<field>', views.download_file, name='download-file'),
     path('', include(router.urls)),
 ] + generate_routes({'flower-proxy': {'base_url': 'http://flower:5555/', 'prefix': '/flower/'}})
 
--- rgd/geodata/api/download.py
+++ rgd/geodata/api/download.py
@@ -1,42 +1,10 @@
-import os
-
-from django.db.models.fields.files import FieldFile
-from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
+from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404  # , render
-from django.utils.encoding import smart_str
 from drf_yasg.utils import swagger_auto_schema
 from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 from rgd.geodata import models
-
-
-@swagger_auto_schema(
-    method='GET',
-    operation_summary='Download a model file',
-    operation_description='Download a model file through the server instead of from the assetstore',
-)
-@api_view(['GET'])
-def download_file(request, model, id, field):
-    model_class = ''.join([part[:1].upper() + part[1:] for part in model.split('_')])
-    if not hasattr(models, model_class):
-        raise AttributeError('No such model (%s)' % model)
-    model_inst = get_object_or_404(getattr(models, model_class), pk=id)
-    if not isinstance(getattr(model_inst, field, None), FieldFile):
-        raise AttributeError('No such file (%s)' % field)
-    file = getattr(model_inst, field)
-    filename = os.path.basename(file.name)
-    if not filename:
-        filename = '%s_%s_%s.dat' % (model, id, field)
-    mimetype = getattr(
-        model_inst,
-        '%s_mimetype' % field,
-        'text/plain' if field == 'log' else 'application/octet-stream',
-    )
-    response = HttpResponse(file.chunks(), content_type=mimetype)
-    response['Content-Disposition'] = smart_str(u'attachment; filename=%s' % filename)
-    if len(file) is not None:
-        response['Content-Length'] = len(file)
-    return response
 
 
 @swagger_auto_schema(
@@ -85,7 +53,7 @@ def _get_status_response(request, model, pk):
         'model': model,
         'status': instance.status,
     }
-    return JsonResponse(data)
+    return Response(data)
 
 
 @swagger_auto_schema(
