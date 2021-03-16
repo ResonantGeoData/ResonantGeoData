ad0447b298cfdd6ef3322e7fbda9bd0e131e96c9
banesullivan <bane.sullivan@kitware.com>
2021-03-16 10:46:34 -0600

    Improve task launching and management in demo commands

--- rgd/geodata/management/commands/_data_helper.py
+++ rgd/geodata/management/commands/_data_helper.py
@@ -3,15 +3,36 @@ import logging
 import os
 
 import dateutil.parser
+from django.conf import settings
+from django.core.management.base import BaseCommand
 from django.db.models import Count
 from django.utils.timezone import make_aware
 
-from rgd.geodata import models, tasks
+from rgd.geodata import models
 from rgd.geodata.datastore import datastore, registry
-from rgd.geodata.models.imagery.etl import populate_raster_entry, read_image_file
 from rgd.utility import get_or_create_no_commit, safe_urlopen
 
 logger = logging.getLogger(__name__)
+
+
+class SynchronousTasksCommand(BaseCommand):
+    def set_synchronous(self):
+        # Set celery to run all tasks synchronously
+        self._eager = getattr(settings, 'CELERY_TASK_ALWAYS_EAGER', False)
+        self._prop = getattr(settings, 'CELERY_TASK_EAGER_PROPAGATES', False)
+        settings.CELERY_TASK_ALWAYS_EAGER = True
+        settings.CELERY_TASK_EAGER_PROPAGATES = True
+
+    def reset_celery(self):
+        # Reset celery to previous settings
+        settings.CELERY_TASK_ALWAYS_EAGER = self._eager
+        settings.CELERY_TASK_EAGER_PROPAGATES = self._prop
+
+
+def _save_signal(entry, created):
+    if not created and entry.status == models.mixins.Status.SUCCEEDED:
+        entry.skip_signal = True
+    entry.save()
 
 
 def _get_or_download_checksum_file(name):
@@ -22,39 +43,33 @@ def _get_or_download_checksum_file(name):
             pass  # HACK: see if URL first
         try:
             file_entry = models.ChecksumFile.objects.get(url=name)
-            logger.info('found existing ChecksumFile')
+            _save_signal(file_entry, False)
         except models.ChecksumFile.DoesNotExist:
             file_entry = models.ChecksumFile()
             file_entry.url = name
             file_entry.type = models.FileSourceType.URL
-            file_entry.save()
-            logger.info('Made new ChecksumFile')
+            _save_signal(file_entry, True)
     except ValueError:
         sha = registry[name].split(':')[1]  # NOTE: assumes sha512
         try:
             file_entry = models.ChecksumFile.objects.get(checksum=sha)
+            _save_signal(file_entry, False)
         except models.ChecksumFile.DoesNotExist:
             path = datastore.fetch(name)
             file_entry = models.ChecksumFile()
             file_entry.name = name
             file_entry.file.save(os.path.basename(path), open(path, 'rb'))
             file_entry.type = models.FileSourceType.FILE_FIELD
-            file_entry.save()
-            tasks.task_checksum_file_post_save.delay(file_entry.id)
+            _save_signal(file_entry, True)
     return file_entry
 
 
-def _get_or_create_file_model(model, name, skip_signal=False):
+def _get_or_create_file_model(model, name):
     # For models that point to a `ChecksumFile`
     file_entry = _get_or_download_checksum_file(name)
     # No commit in case we need to skip the signal
     entry, created = get_or_create_no_commit(model, file=file_entry)
-    logger.info(f'Created new associated file model: {created}')
-    # In case the last population failed
-    if skip_signal:
-        entry.skip_signal = True
-    if created or entry.status != models.mixins.Status.SUCCEEDED:
-        entry.save()
+    _save_signal(entry, created)
     return entry
 
 
@@ -65,18 +80,7 @@ def load_image_files(image_files):
             result = load_image_files(imfile)
         else:
             # Run `read_image_file` sequentially to ensure `ImageEntry` is generated
-            entry = _get_or_create_file_model(models.ImageFile, imfile, skip_signal=True)
-            if entry.status != models.mixins.Status.SUCCEEDED:
-                # Run init task in sequence to create associated ImageEntry
-                read_image_file(entry)
-                entry.status = models.mixins.Status.SUCCEEDED
-                entry.failure_reason = ''
-                entry.save(
-                    update_fields=[
-                        'status',
-                        'failure_reason',
-                    ]
-                )
+            entry = _get_or_create_file_model(models.ImageFile, imfile)
             result = entry.imageentry.pk
         ids.append(result)
     return ids
@@ -97,7 +101,9 @@ def load_raster_files(raster_files, dates=None, names=None):
         raster_files = files
 
     ids = []
+    count = len(raster_files)
     for i, rf in enumerate(raster_files):
+        logger.info(f'Processesing raster {i+1} of {count}')
         imentries = load_image_files(
             [
                 rf,
@@ -123,19 +129,7 @@ def load_raster_files(raster_files, dates=None, names=None):
                 imset.save()
             # Make raster of that image set
             raster, created = get_or_create_no_commit(models.RasterEntry, image_set=imset)
-            raster.skip_signal = True
-            raster.save()
-            if raster.status != models.mixins.Status.SUCCEEDED:
-                # Run init task in sequence to create associated rastermetaentry
-                populate_raster_entry(raster)
-                raster.status = models.mixins.Status.SUCCEEDED
-                raster.failure_reason = ''
-                raster.save(
-                    update_fields=[
-                        'status',
-                        'failure_reason',
-                    ]
-                )
+            _save_signal(raster, created)
             if dates:
                 adt = dateutil.parser.isoparser().isoparse(dates[i])
                 raster.rastermetaentry.acquisition_date = make_aware(adt)
@@ -173,7 +167,9 @@ def load_kwcoco_archives(archives):
     for fspec, farch in archives:
         spec = _get_or_download_checksum_file(fspec)
         arch = _get_or_download_checksum_file(farch)
-        ds, _ = get_or_create_no_commit(models.KWCOCOArchive, spec_file=spec, image_archive=arch)
-        ds.save()
+        ds, created = get_or_create_no_commit(
+            models.KWCOCOArchive, spec_file=spec, image_archive=arch
+        )
+        _save_signal(ds, created)
         ids.append(ds.id)
     return ids
--- rgd/geodata/management/commands/landsat_data.py
+++ rgd/geodata/management/commands/landsat_data.py
@@ -1,9 +1,6 @@
 import json
 import os
 
-from django.conf import settings
-from django.core.management.base import BaseCommand  # , CommandError
-
 from . import _data_helper as helper
 
 SUCCESS_MSG = 'Finished loading all landsat data.'
@@ -22,7 +19,7 @@ def _get_landsat_urls(count):
     return urls
 
 
-class Command(BaseCommand):
+class Command(helper.SynchronousTasksCommand):
     help = 'Populate database with demo landsat data from S3.'
 
     def add_arguments(self, parser):
@@ -32,11 +29,7 @@ class Command(BaseCommand):
         )
 
     def handle(self, *args, **options):
-        # Set celery to run all tasks synchronously
-        eager = getattr(settings, 'CELERY_TASK_ALWAYS_EAGER', False)
-        prop = getattr(settings, 'CELERY_TASK_EAGER_PROPAGATES', False)
-        # settings.CELERY_TASK_ALWAYS_EAGER = True
-        # settings.CELERY_TASK_EAGER_PROPAGATES = True
+        self.set_synchronous()
 
         if options.get('get_count', False):
             n = len(_get_landsat_urls(0))
@@ -48,7 +41,4 @@ class Command(BaseCommand):
         # Run the command
         helper.load_raster_files(_get_landsat_urls(count))
         self.stdout.write(self.style.SUCCESS(SUCCESS_MSG))
-
-        # Reset celery to previous settings
-        settings.CELERY_TASK_ALWAYS_EAGER = eager
-        settings.CELERY_TASK_EAGER_PROPAGATES = prop
+        self.reset_celery()
