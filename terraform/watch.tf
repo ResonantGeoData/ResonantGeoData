58f229059064cbe8d56b4e524533265484f2e144
Bane Sullivan <bane.sullivan@kitware.com>
2021-04-13 10:22:51 -0600

    Add AWS CNAME record for WATCH (#350)
    * Add AWS CNAME record for WATCH
    
    * Fix typo
    
    * Fix zone_id arg
    
    * Rename to watch.tf

---
+++ terraform/watch.tf
@@ -0,0 +1,7 @@
+resource "aws_route53_record" "watch" {
+  name             = "_1665444c5f45134012652f2b83b63432.watch.resonantgeodata.com"
+  type             = "CNAME"
+  ttl              = "300"
+  zone_id          = aws_route53_zone.common.id
+  records          = ["_4e20c5eb5edd4ea6f2b5f5c66ce76409.bbfvkzsszw.acm-validations.aws."]
+}
