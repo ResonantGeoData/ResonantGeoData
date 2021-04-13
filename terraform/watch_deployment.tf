72ddd198fc6032228b737f6103dee13e18751455
Bane Sullivan <bane.sullivan@kitware.com>
2021-04-13 09:37:14 -0600

    Fix zone_id arg

--- terraform/watch_deployment.tf
+++ terraform/watch_deployment.tf
@@ -2,6 +2,6 @@ resource "aws_route53_record" "watch" {
   name             = "_1665444c5f45134012652f2b83b63432.watch.resonantgeodata.com"
   type             = "CNAME"
   ttl              = "300"
-  route53_zone_id  = aws_route53_zone.common.id
+  zone_id          = aws_route53_zone.common.id
   records          = ["_4e20c5eb5edd4ea6f2b5f5c66ce76409.bbfvkzsszw.acm-validations.aws."]
 }
