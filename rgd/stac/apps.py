4bf3e731b51e7203ed743c3006a40aec1f86e6ae
Matt Covalt <mcovalt@mailbox.org>
2021-04-28 23:29:19 -0400

    Fix apps.py

--- rgd/stac/apps.py
+++ rgd/stac/apps.py
@@ -1,6 +1,12 @@
 from django.apps import AppConfig
+from django.core.checks import register
 
 
 class STACConfig(AppConfig):
     name = 'rgd.stac'
-    verbose_name = 'SpatioTemporal Asset Catalog '
+    verbose_name = 'SpatioTemporal Asset Catalog'
+
+    def ready(self):
+        from rgd.stac.models.extensions import check_model_extensions
+
+        register(check_model_extensions)
