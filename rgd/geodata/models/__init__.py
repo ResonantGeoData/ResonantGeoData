c54588602003e226b3d030a105b070b3455e9730
banesullivan <bane.sullivan@kitware.com>
2021-05-12 11:28:57 -0600

    Add new email whitelist table

--- rgd/geodata/models/__init__.py
+++ rgd/geodata/models/__init__.py
@@ -1,6 +1,12 @@
 # These are models we want to expose in the top-level model namespace
 from .collection import Collection, CollectionMembership  # noqa
-from .common import ChecksumFile, FileSourceType, ModifiableEntry, SpatialEntry  # noqa
+from .common import (  # noqa
+    ChecksumFile,
+    FileSourceType,
+    ModifiableEntry,
+    SpatialEntry,
+    WhitelistedEmail,
+)
 from .fmv import *  # noqa
 from .geometry import *  # noqa
 from .imagery import *  # noqa
