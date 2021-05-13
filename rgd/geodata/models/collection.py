5789ceede28872a77ad01ff2a5c6f8f10e0a3dd5
Matt Covalt <mcovalt@mailbox.org>
2021-05-13 12:28:13 -0400

    Remove pseudo 'users' field on 'Collection'

--- rgd/geodata/models/collection.py
+++ rgd/geodata/models/collection.py
@@ -4,7 +4,6 @@ from django.db import models
 
 class Collection(models.Model):
     name = models.CharField(max_length=127)
-    users = models.ManyToManyField(settings.AUTH_USER_MODEL, through='CollectionMembership')
 
     def __str__(self):
         return self.name
