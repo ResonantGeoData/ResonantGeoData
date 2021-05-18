4247d647f1386a86423b7a4074ea564627a70e36
banesullivan <bane.sullivan@kitware.com>
2021-05-18 12:45:35 -0600

    Update python client methods

--- rgdc/rgdc/rgdc.py
+++ rgdc/rgdc/rgdc.py
@@ -11,7 +11,7 @@ from geomet import wkt
 from tqdm import tqdm
 
 from .session import RgdcSession
-from .types import DATETIME_OR_STR_TUPLE, SEARCH_DATATYPE_CHOICE, SEARCH_PREDICATE_CHOICE
+from .types import DATETIME_OR_STR_TUPLE, SEARCH_PREDICATE_CHOICE
 from .utils import (
     DEFAULT_RGD_API,
     datetime_to_str,
@@ -206,14 +206,7 @@ class Rgdc:
         relates: Optional[str] = None,
         distance: Optional[Tuple[float, float]] = None,
         acquired: Optional[DATETIME_OR_STR_TUPLE] = None,
-        created: Optional[DATETIME_OR_STR_TUPLE] = None,
-        modified: Optional[DATETIME_OR_STR_TUPLE] = None,
-        datatype: Optional[SEARCH_DATATYPE_CHOICE] = None,
         instrumentation: Optional[str] = None,
-        num_bands: Optional[Tuple[int, int]] = None,
-        resolution: Optional[Tuple[int, int]] = None,
-        cloud_cover: Optional[Tuple[float, float]] = None,
-        frame_rate: Optional[Tuple[int, int]] = None,
         limit: Optional[int] = None,
         offset: Optional[int] = None,
     ) -> List[Dict]:
@@ -231,14 +224,7 @@ class Rgdc:
                 DE-9IM string code.
             distance: The min/max distance around the queried geometry in meters.
             acquired: The min/max date and time (ISO 8601) when data was acquired.
-            created: The min/max date and time (ISO 8601) when data was created.
-            modified: The min/max date and time (ISO 8601) when data was modified.
-            datatype: The datatype to provide.
             instrumentation: The instrumentation used to acquire at least one of these data.
-            num_bands: The min/max number of bands in the raster.
-            resolution: The min/max resolution of the raster.
-            cloud_cover: The min/max cloud coverage of the raster.
-            frame_rate: The min/max frame rate of the video.
             limit: The maximum number of results to return.
             offset: The number of results to skip.
 
@@ -250,7 +236,6 @@ class Rgdc:
         params = {
             'predicate': predicate,
             'relates': relates,
-            'datatype': datatype,
             'instrumentation': instrumentation,
             'limit': limit,
             'offset': offset,
@@ -282,15 +267,81 @@ class Rgdc:
             params['acquired_before'] = datetime_to_str(amax)
             params['acquired_after'] = datetime_to_str(amin)
 
-        if created and len(created) == 2:
-            cmin, cmax = created
-            params['created_before'] = datetime_to_str(cmax)
-            params['created_after'] = datetime_to_str(cmin)
+        return list(limit_offset_pager(self.session, 'geosearch', params=params))
 
-        if modified and len(modified) == 2:
-            mmin, mmax = modified
-            params['modified_before'] = datetime_to_str(mmax)
-            params['modified_after'] = datetime_to_str(mmin)
+    def search_raster_stac(
+        self,
+        query: Optional[Union[Dict, str]] = None,
+        predicate: Optional[SEARCH_PREDICATE_CHOICE] = None,
+        relates: Optional[str] = None,
+        distance: Optional[Tuple[float, float]] = None,
+        acquired: Optional[DATETIME_OR_STR_TUPLE] = None,
+        instrumentation: Optional[str] = None,
+        num_bands: Optional[Tuple[int, int]] = None,
+        resolution: Optional[Tuple[int, int]] = None,
+        cloud_cover: Optional[Tuple[float, float]] = None,
+        limit: Optional[int] = None,
+        offset: Optional[int] = None,
+    ) -> List[Dict]:
+        """
+        Search for raster entries based on various criteria.
+
+        For Ranges (Tuples), an entry of `None` means that side of the range is unbounded.
+        E.g. a range of (2, None) is 2 or more, (None, 5) is at most 5, (2, 5) is between 2 and 5.
+
+        Args:
+            query: Either a WKT GeoJSON representation, a GeoJSON string, or a GeoJSON dict.
+            predicate: A named spatial predicate based on the DE-9IM. This spatial predicate will
+                be used to filter data such that predicate(a, b) where b is the queried geometry.
+            relates: Specify exactly how the queried geometry should relate to the data using a
+                DE-9IM string code.
+            distance: The min/max distance around the queried geometry in meters.
+            acquired: The min/max date and time (ISO 8601) when data was acquired.
+            instrumentation: The instrumentation used to acquire at least one of these data.
+            num_bands: The min/max number of bands in the raster.
+            resolution: The min/max resolution of the raster.
+            cloud_cover: The min/max cloud coverage of the raster.
+            limit: The maximum number of results to return.
+            offset: The number of results to skip.
+
+        Returns:
+            A list of Spatial Entries in STAC Item format.
+        """
+        # The dict that will be used to store params.
+        # Initialize with queries that won't be additionally processed.
+        params = {
+            'predicate': predicate,
+            'relates': relates,
+            'instrumentation': instrumentation,
+            'limit': limit,
+            'offset': offset,
+        }
+
+        if query:
+            if isinstance(query, str):
+                try:
+                    query = json.loads(query)
+                except JSONDecodeError:
+                    pass
+
+            if isinstance(query, dict):
+                # Allow failure on invalid format
+                query = wkt.dumps(query)
+
+            params['q'] = query
+
+        # Process range params
+
+        if distance and len(distance) == 2:
+            dmin, dmax = distance
+            params['distance_min'] = dmin
+            params['distance_max'] = dmax
+
+        # TODO: Determine if the before/after param order needs to be swapped?
+        if acquired and len(acquired) == 2:
+            amin, amax = acquired
+            params['acquired_before'] = datetime_to_str(amax)
+            params['acquired_after'] = datetime_to_str(amin)
 
         if num_bands and len(num_bands) == 2:
             nbmin, nbmax = num_bands
@@ -307,9 +358,4 @@ class Rgdc:
             params['cloud_cover_min'] = ccmin
             params['cloud_cover_max'] = ccmax
 
-        if frame_rate and len(frame_rate) == 2:
-            frmin, frmax = frame_rate
-            params['frame_rate_min'] = frmin
-            params['frame_rate_max'] = frmax
-
-        return list(limit_offset_pager(self.session, 'geosearch', params=params))
+        return list(limit_offset_pager(self.session, 'geosearch/raster', params=params))
