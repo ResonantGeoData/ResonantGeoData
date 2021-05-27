c257c954d100337f6527c0d07d70372e748c12ae
banesullivan <bane.sullivan@kitware.com>
2021-05-27 11:41:58 -0600

    Fix import

--- rgd/geodata/models/imagery/__init__.py
+++ rgd/geodata/models/imagery/__init__.py
@@ -8,7 +8,7 @@ type should be added, subclassing the appropriate base models.
 from .annotation import Annotation, PolygonSegmentation, RLESegmentation, Segmentation
 
 # flake8: noqa
-from .base import BandMetaEntry, ImageEntry, ImageFile, ImageSet
+from .base import BandMetaEntry, ImageEntry, ImageFile, ImageSet, ImageSetSpatial
 from .kwcoco import KWCOCOArchive
 from .processed import ConvertedImageFile, SubsampledImage
 from .raster import RasterEntry, RasterMetaEntry
