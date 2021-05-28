e75072714c76fe88ad765f3caf34216bc726810f
banesullivan <bane.sullivan@kitware.com>
2021-05-28 10:16:25 -0600

    Linting

--- rgd/geodata/views.py
+++ rgd/geodata/views.py
@@ -118,7 +118,9 @@ class StatisticsView(generic.ListView):
         context['acquisition_date__max'] = dates['acquisition_date__max']
         extent = q.aggregate(Extent('outline'))
         if not extent['outline__extent']:
-            extent['outline__extent'] = [None,] * 4
+            extent['outline__extent'] = [
+                None,
+            ] * 4
         context['extents'] = json.dumps(
             {
                 'xmin': extent['outline__extent'][0],
