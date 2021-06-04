a0f909c70fb710426373fdbc4043c3d651700a9c
banesullivan <bane.sullivan@kitware.com>
2021-06-04 14:14:36 -0600

    Add basic test

--- rgd/geodata/tests/test_stac.py
+++ rgd/geodata/tests/test_stac.py
@@ -1,5 +1,7 @@
 import pytest
 
+from rgd.geodata.serializers import STACRasterSerializer
+
 
 def _check_conforms_stac(data):
     assert 'stac_version' in data
@@ -29,3 +31,23 @@ def test_raster_stac_serializer_multi_file_bands(admin_api_client, sample_raster
     _check_conforms_stac(data)
     assets = data['assets']
     assert len(assets) == 3
+
+
+@pytest.mark.django_db(transaction=True)
+def test_raster_stac_export_import(admin_api_client, sample_raster_a):
+    data = STACRasterSerializer(sample_raster_a).data
+    instance = STACRasterSerializer().create(data)
+    assert instance
+    # TODO: versify instance == sample_raster_a
+
+    # Same for ImageFile/ImageEntry
+    assert instance.parent_raster.image_set.images.count() == sample_raster_a.parent_raster.image_set.images.count()
+
+    # Same for ancillary files
+    assert instance.parent_raster.ancillary_files.count() == sample_raster_a.parent_raster.ancillary_files.count()
+
+    # Get checksum files and make sure they are actually the same (no duplicates)
+
+    # Asset outline/footprint of rasters are the same
+    assert instance.footprint == sample_raster_a.footprint
+    assert instance.outline == sample_raster_a.outline
