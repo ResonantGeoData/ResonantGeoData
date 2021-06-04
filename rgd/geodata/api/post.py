2a3941f590ef7da1ad0bf8710373ae2858360009
banesullivan <bane.sullivan@kitware.com>
2021-06-04 15:39:22 -0600

    Add POST endpoint

--- rgd/geodata/api/post.py
+++ rgd/geodata/api/post.py
@@ -1,14 +1,19 @@
 from rest_framework.generics import CreateAPIView
 
 from .. import serializers
-from ..models.imagery import ConvertedImageFile, SubsampledImage
+from .. import models
 
 
 class CreateConvertedImageFile(CreateAPIView):
-    queryset = ConvertedImageFile.objects.all()
+    queryset = models.ConvertedImageFile.objects.all()
     serializer_class = serializers.ConvertedImageFileSerializer
 
 
 class CreateSubsampledImage(CreateAPIView):
-    queryset = SubsampledImage.objects.all()
+    queryset = models.SubsampledImage.objects.all()
     serializer_class = serializers.SubsampledImageSerializer
+
+
+class CreateRasterSTAC(CreateAPIView):
+    queryset = models.RasterMetaEntry.objects.all()
+    serializer_class = serializers.STACRasterSerializer
