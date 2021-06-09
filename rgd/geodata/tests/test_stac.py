972eb4a9d63f04f6f870c7a6a4f66d59e94f1b9f
Matt Covalt <mcovalt@mailbox.org>
2021-06-09 18:36:38 -0400

    Add STAC ingestion on serializer (#416)
    * Add pystac validation package
    
    * Add STAC ingestion routine
    
    * Update rgd/geodata/serializers.py
    
    Co-authored-by: Bane Sullivan <bane.sullivan@kitware.com>
    
    * Add basic test
    
    * [WIP] better handle STAC Item ingest
    
    * Add new URL raster data fixture
    
    * Improve STAC serializer
    
    * Improve STAC tests
    
    * Use pystac to validate existing serializer
    
    * Add POST endpoint
    
    * Move KWCOCO ETL code to seperate module
    
    * Move rasterio and shapely to be required depends
    
    * Disable pystac validation
    
    * Formatting
    
    * Fix ConvertedImageFile cleanup bug
    
    * Allow for single asset STAC Items with no roles
    
    * Fix bug
    
    Co-authored-by: Bane Sullivan <bane.sullivan@kitware.com>

--- rgd/geodata/tests/test_stac.py
+++ rgd/geodata/tests/test_stac.py
@@ -1,10 +1,17 @@
+import pystac
 import pytest
+
+from rgd.geodata.serializers import STACRasterSerializer
 
 
 def _check_conforms_stac(data):
     assert 'stac_version' in data
     assert 'assets' in data
-    # TODO: make this better
+    item = pystac.Item.from_dict(data)  # noqa
+    # TODO:
+    # errors = item.validate()
+    # if errors:
+    #     raise ValueError('ilformed STAC Item.')
 
 
 @pytest.mark.django_db(transaction=True)
@@ -29,3 +36,33 @@ def test_raster_stac_serializer_multi_file_bands(admin_api_client, sample_raster
     _check_conforms_stac(data)
     assets = data['assets']
     assert len(assets) == 3
+
+
+@pytest.mark.django_db(transaction=True)
+def test_raster_stac_export_import(admin_api_client, sample_raster_url):
+    sample = sample_raster_url
+    data = STACRasterSerializer(sample).data
+    instance = STACRasterSerializer().create(data)
+    # Check ImageFile/ImageEntry
+    assert (
+        instance.parent_raster.image_set.images.count()
+        == sample.parent_raster.image_set.images.count()
+    )
+    # Since testing URL files, make sure there are no duplicates
+    assert set(instance.parent_raster.image_set.images.all()) == set(
+        sample.parent_raster.image_set.images.all()
+    )
+    # Get checksum files and make sure they are actually the same (no duplicates)
+
+    # Same for ancillary files
+    assert (
+        instance.parent_raster.ancillary_files.count()
+        == sample.parent_raster.ancillary_files.count()
+    )
+    assert set(instance.parent_raster.ancillary_files.all()) == set(
+        sample.parent_raster.ancillary_files.all()
+    )
+
+    # Asset outline/footprint of rasters are the same
+    assert instance.footprint.equals(sample.footprint)
+    # assert instance.outline.equals(sample.outline)
