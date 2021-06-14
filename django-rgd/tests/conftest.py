ad3fb7ce223345f3be1b2e2146d6a0c06e9a15b9
banesullivan <bane.sullivan@kitware.com>
2021-06-14 15:37:51 -0600

    Fix conftest

--- django-rgd/tests/conftest.py
+++ django-rgd/tests/conftest.py
@@ -5,6 +5,7 @@ from pytest_factoryboy import register
 
 from rgd_testing_utils import factories
 from rgd_testing_utils.data_fixtures import *  # noqa
+from rgd_testing_utils.api_fixtures import *  # noqa
 
 
 for _, fac in inspect.getmembers(factories):
