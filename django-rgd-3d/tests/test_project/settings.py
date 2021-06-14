4d58969f820ff54e6502466115d1c6945fd22c3c
banesullivan <bane.sullivan@kitware.com>
2021-06-14 16:05:39 -0600

    Remove WSGI from test project

--- django-rgd-3d/tests/test_project/settings.py
+++ django-rgd-3d/tests/test_project/settings.py
@@ -6,7 +6,6 @@ INSTALLED_APPS += [  # noqa
 ]
 
 ROOT_URLCONF = 'test_project.urls'
-WSGI_APPLICATION = 'test_project.wsgi.application'
 
 CELERY_TASK_ALWAYS_EAGER = True
 CELERY_TASK_EAGER_PROPAGATES = True
--- django-rgd/tests/test_project/settings.py
+++ django-rgd/tests/test_project/settings.py
@@ -1,7 +1,6 @@
 from rgd_testing_utils.settings import *  # noqa
 
 ROOT_URLCONF = 'test_project.urls'
-WSGI_APPLICATION = 'test_project.wsgi.application'
 
 CELERY_TASK_ALWAYS_EAGER = True
 CELERY_TASK_EAGER_PROPAGATES = True
