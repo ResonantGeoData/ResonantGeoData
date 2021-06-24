687348c6a69b75373ea2e86ca8c1f17b10a81361
banesullivan <bane.sullivan@kitware.com>
2021-06-24 12:39:34 -0600

    Bump version

--- version.py
+++ version.py
@@ -15,7 +15,7 @@ Which denotes the first release candidate.
 
 """
 # major, minor, patch
-version_info = 0, 2, '0rc1'
+version_info = 0, 2, '0rc2'
 
 # Nice string for the version
 __version__ = '.'.join(map(str, version_info))
