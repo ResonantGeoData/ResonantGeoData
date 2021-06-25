126cf1f88694f89f3b6c083d555616b2ee6da191
banesullivan <bane.sullivan@kitware.com>
2021-06-25 09:28:30 -0600

    Get version of each package seperately

--- django-rgd-3d/rgd_3d/__init__.py
+++ django-rgd-3d/rgd_3d/__init__.py
@@ -1,1 +1,7 @@
-from rgd import __version__  # noqa
+from pkg_resources import DistributionNotFound, get_distribution
+
+try:
+    __version__ = get_distribution('django-rgd-3d').version
+except DistributionNotFound:
+    # package is not installed
+    __version__ = None
--- django-rgd-geometry/rgd_geometry/__init__.py
+++ django-rgd-geometry/rgd_geometry/__init__.py
@@ -1,1 +1,7 @@
-from rgd import __version__  # noqa
+from pkg_resources import DistributionNotFound, get_distribution
+
+try:
+    __version__ = get_distribution('django-rgd-geometry').version
+except DistributionNotFound:
+    # package is not installed
+    __version__ = None
