acf07aeb9ff28d0ff6254752d30a7b6658fdcaa8
banesullivan <bane.sullivan@kitware.com>
2021-06-29 11:22:44 -0600

    Use redirect on homepage for example app

--- example_project/rgd_example/urls.py
+++ example_project/rgd_example/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import include, path, re_path
+from django.views.generic.base import RedirectView
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 from rest_framework import permissions
@@ -14,6 +15,8 @@ urlpatterns = [
     path('', include('rgd_fmv.urls')),
     path('', include('rgd_geometry.urls')),
     path('', include('rgd_imagery.urls')),
+    # Redirect homepage to RGD core app homepage
+    path(r'', RedirectView.as_view(url='rgd', permanent=False), name='index'),
 ]
 
 schema_view = get_schema_view(
