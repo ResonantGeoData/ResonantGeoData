9c5471b9aa943358e487452bafe8f87b3f277096
Bane Sullivan <bane.sullivan@kitware.com>
2021-06-30 19:30:58 -0600

    Patch ImageSet admin action (#448)
    * Patch ImageSet admin action
    
    * Clean up

--- django-rgd-imagery/rgd_imagery/admin/base.py
+++ django-rgd-imagery/rgd_imagery/admin/base.py
@@ -87,9 +87,8 @@ def make_raster_for_each_image(modeladmin, request, queryset):
 
 def clean_empty_image_sets(modeladmin, request, queryset):
     """Delete empty `ImageSet`s."""
-    for imset in queryset.all():
-        if len(imset.images) < 1:
-            imset.delete()
+    q = queryset.filter(images=None)
+    q.delete()
 
 
 def convert_images(modeladmin, request, queryset):
