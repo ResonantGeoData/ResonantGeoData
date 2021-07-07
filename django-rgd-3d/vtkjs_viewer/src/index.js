2bc9c84905574906ac68df055587d5b3d0686c3a
Bane Sullivan <bane.sullivan@kitware.com>
2021-07-07 17:13:07 -0600

    New and improved 3D Viewer (#455)
    * Create first pass at 3D viewer app
    
    * Update to latest vtk.js
    
    * Hacky fix to viewer layout
    
    * JS linting
    
    * Improve layouts
    
    * Add point size slider
    
    * Add RGB coloring to viewer

---
+++ django-rgd-3d/vtkjs_viewer/src/index.js
@@ -0,0 +1,443 @@
+// Load the rendering pieces we want to use (for both WebGL and WebGPU)
+import '@kitware/vtk.js/Rendering/Profiles/Geometry';
+
+import { debounce } from '@kitware/vtk.js/macro';
+import vtkActor from '@kitware/vtk.js/Rendering/Core/Actor';
+import vtkDataArray from '@kitware/vtk.js/Common/Core/DataArray';
+import vtkScalarBarActor from '@kitware/vtk.js/Rendering/Core/ScalarBarActor';
+import vtkColorMaps from '@kitware/vtk.js/Rendering/Core/ColorTransferFunction/ColorMaps';
+import vtkColorTransferFunction from '@kitware/vtk.js/Rendering/Core/ColorTransferFunction';
+import vtkFullScreenRenderWindow from '@kitware/vtk.js/Rendering/Misc/FullScreenRenderWindow';
+import vtkMapper from '@kitware/vtk.js/Rendering/Core/Mapper';
+import vtkURLExtract from '@kitware/vtk.js/Common/Core/URLExtract';
+import vtkXMLPolyDataReader from '@kitware/vtk.js/IO/XML/XMLPolyDataReader';
+import vtkFPSMonitor from '@kitware/vtk.js/Interaction/UI/FPSMonitor';
+import Base64 from '@kitware/vtk.js/Common/Core/Base64';
+
+// Force DataAccessHelper to have access to various data source
+import '@kitware/vtk.js/IO/Core/DataAccessHelper/HtmlDataAccessHelper';
+import '@kitware/vtk.js/IO/Core/DataAccessHelper/JSZipDataAccessHelper';
+
+import {
+  ColorMode,
+  ScalarMode
+} from '@kitware/vtk.js/Rendering/Core/Mapper/Constants';
+
+import style from './GeometryViewer.module.css';
+import icon from './favicon-96x96.png';
+
+let background = [0, 0, 0];
+let renderWindow;
+let renderer;
+let scalarBarActor;
+
+global.pipeline = {};
+
+// Process arguments from URL
+const userParams = vtkURLExtract.extractURLParameters();
+
+// Background handling
+if (userParams.background) {
+  background = userParams.background.split(',').map((s) => Number(s));
+}
+const selectorClass =
+  background.length === 3 && background.reduce((a, b) => a + b, 0) < 1.5
+    ? style.dark
+    : style.light;
+
+// lut
+const lutName = userParams.lut || 'erdc_rainbow_bright';
+
+// field
+const field = userParams.field || '';
+
+// camera
+function updateCamera (camera) {
+  ['zoom', 'pitch', 'elevation', 'yaw', 'azimuth', 'roll', 'dolly'].forEach(
+    (key) => {
+      if (userParams[key]) {
+        camera[key](userParams[key]);
+      }
+      renderWindow.render();
+    }
+  );
+}
+global.updateCamera = updateCamera;
+
+// ----------------------------------------------------------------------------
+// DOM containers for UI control
+// ----------------------------------------------------------------------------
+
+const rootControllerContainer = document.createElement('div');
+rootControllerContainer.setAttribute('class', style.rootController);
+
+const addDataSetButton = document.createElement('img');
+addDataSetButton.setAttribute('class', style.button);
+addDataSetButton.setAttribute('src', icon);
+addDataSetButton.addEventListener('click', () => {
+  const isVisible = rootControllerContainer.style.display !== 'none';
+  rootControllerContainer.style.display = isVisible ? 'none' : 'flex';
+});
+
+const fpsMonitor = vtkFPSMonitor.newInstance();
+const fpsElm = fpsMonitor.getFpsMonitorContainer();
+fpsElm.classList.add(style.fpsMonitor);
+
+// ----------------------------------------------------------------------------
+// Add class to body if iOS device
+// ----------------------------------------------------------------------------
+
+const iOS = /iPad|iPhone|iPod/.test(window.navigator.platform);
+
+if (iOS) {
+  document.querySelector('body').classList.add('is-ios-device');
+}
+
+// ----------------------------------------------------------------------------
+
+function emptyContainer (container) {
+  fpsMonitor.setContainer(null);
+  while (container.firstChild) {
+    container.removeChild(container.firstChild);
+  }
+}
+
+global.emptyContainer = emptyContainer;
+// ----------------------------------------------------------------------------
+
+function createViewer (container) {
+  const fullScreenRenderer = vtkFullScreenRenderWindow.newInstance({
+    background,
+    rootContainer: container,
+    containerStyle: { height: '100%', width: '100%', position: 'absolute' }
+  });
+  renderer = fullScreenRenderer.getRenderer();
+  renderWindow = fullScreenRenderer.getRenderWindow();
+
+  global.fullScreenRenderer = fullScreenRenderer;
+  global.renderer = renderer;
+  global.renderWindow = renderWindow;
+
+  renderWindow.getInteractor().setDesiredUpdateRate(15);
+
+  container.appendChild(rootControllerContainer);
+  container.appendChild(addDataSetButton);
+
+  scalarBarActor = vtkScalarBarActor.newInstance();
+  renderer.addActor(scalarBarActor);
+
+  if (userParams.fps) {
+    if (Array.isArray(userParams.fps)) {
+      fpsMonitor.setMonitorVisibility(...userParams.fps);
+      if (userParams.fps.length === 4) {
+        fpsMonitor.setOrientation(userParams.fps[3]);
+      }
+    }
+    fpsMonitor.setRenderWindow(renderWindow);
+    fpsMonitor.setContainer(container);
+    fullScreenRenderer.setResizeCallback(fpsMonitor.update);
+  }
+}
+global.createViewer = createViewer;
+
+// ----------------------------------------------------------------------------
+
+function createPipeline (fileContents, name = undefined) {
+  // Create UI
+  const presetSelector = document.createElement('select');
+  presetSelector.setAttribute('class', selectorClass);
+  presetSelector.innerHTML = vtkColorMaps.rgbPresetNames
+    .map(
+      (name) =>
+        `<option value="${name}" ${
+          lutName === name ? 'selected="selected"' : ''
+        }>${name}</option>`
+    )
+    .join('');
+
+  const representationSelector = document.createElement('select');
+  representationSelector.setAttribute('class', selectorClass);
+  representationSelector.innerHTML = [
+    'Hidden',
+    'Points',
+    'Wireframe',
+    'Surface',
+    'Surface with Edge'
+  ]
+    .map(
+      (name, idx) =>
+        `<option value="${idx === 0 ? 0 : 1}:${idx < 4 ? idx - 1 : 2}:${
+          idx === 4 ? 1 : 0
+        }">${name}</option>`
+    )
+    .join('');
+  representationSelector.value = '1:2:0';
+
+  const colorBySelector = document.createElement('select');
+  colorBySelector.setAttribute('class', selectorClass);
+
+  const componentSelector = document.createElement('select');
+  componentSelector.setAttribute('class', selectorClass);
+  componentSelector.style.display = 'none';
+
+  const opacitySelector = document.createElement('input');
+  opacitySelector.setAttribute('class', selectorClass);
+  opacitySelector.setAttribute('type', 'range');
+  opacitySelector.setAttribute('value', '100');
+  opacitySelector.setAttribute('max', '100');
+  opacitySelector.setAttribute('min', '1');
+
+  const pointSizeSelector = document.createElement('input');
+  pointSizeSelector.setAttribute('class', selectorClass);
+  pointSizeSelector.setAttribute('type', 'range');
+  pointSizeSelector.setAttribute('value', '1');
+  pointSizeSelector.setAttribute('max', '20');
+  pointSizeSelector.setAttribute('min', '1');
+  pointSizeSelector.style.display = 'none'; // Deafault hidden
+
+  if (name === undefined) {
+    name = 'Mesh';
+  }
+  const labelSelector = document.createElement('label');
+  labelSelector.setAttribute('class', selectorClass);
+  labelSelector.innerHTML = name;
+
+  const controlContainer = document.createElement('div');
+  controlContainer.setAttribute('class', style.control);
+  controlContainer.appendChild(labelSelector);
+  controlContainer.appendChild(representationSelector);
+  controlContainer.appendChild(presetSelector);
+  controlContainer.appendChild(colorBySelector);
+  controlContainer.appendChild(componentSelector);
+  controlContainer.appendChild(opacitySelector);
+  controlContainer.appendChild(pointSizeSelector);
+  rootControllerContainer.appendChild(controlContainer);
+
+  // VTK pipeline
+  const vtpReader = vtkXMLPolyDataReader.newInstance();
+  var buffer = Base64.toArrayBuffer(fileContents);
+  vtpReader.parseAsArrayBuffer(buffer);
+
+  const lookupTable = vtkColorTransferFunction.newInstance();
+  const source = vtpReader.getOutputData(0);
+  const mapper = vtkMapper.newInstance({
+    interpolateScalarsBeforeMapping: false,
+    useLookupTableScalarRange: true,
+    lookupTable,
+    scalarVisibility: false
+  });
+  const actor = vtkActor.newInstance();
+  const scalars = source.getPointData().getScalars();
+  const dataRange = [].concat(scalars ? scalars.getRange() : [0, 1]);
+  let activeArray = vtkDataArray;
+
+  // --------------------------------------------------------------------
+  // Color handling
+  // --------------------------------------------------------------------
+
+  function applyPreset () {
+    const preset = vtkColorMaps.getPresetByName(presetSelector.value);
+    lookupTable.applyColorMap(preset);
+    lookupTable.setMappingRange(dataRange[0], dataRange[1]);
+    lookupTable.updateRange();
+    renderWindow.render();
+  }
+  applyPreset();
+  presetSelector.addEventListener('change', applyPreset);
+
+  // --------------------------------------------------------------------
+  // Representation handling
+  // --------------------------------------------------------------------
+
+  function updateRepresentation (event) {
+    const [
+      visibility,
+      representation,
+      edgeVisibility
+    ] = event.target.value.split(':').map(Number);
+    actor.getProperty().set({ representation, edgeVisibility });
+    actor.setVisibility(!!visibility);
+    renderWindow.render();
+    console.log(representation);
+    if (representation === 0) {
+      // Points === 0
+      pointSizeSelector.style.display = 'block';
+    } else {
+      pointSizeSelector.style.display = 'none';
+    }
+  }
+  representationSelector.addEventListener('change', updateRepresentation);
+
+  // --------------------------------------------------------------------
+  // Opacity handling
+  // --------------------------------------------------------------------
+
+  function updateOpacity (event) {
+    const opacity = Number(event.target.value) / 100;
+    actor.getProperty().setOpacity(opacity);
+    renderWindow.render();
+  }
+
+  opacitySelector.addEventListener('input', updateOpacity);
+
+  // --------------------------------------------------------------------
+  // Point size handling
+  // --------------------------------------------------------------------
+
+  function updatePointSize (event) {
+    const pointSize = Number(event.target.value);
+    actor.getProperty().setPointSize(pointSize);
+    renderWindow.render();
+  }
+
+  pointSizeSelector.addEventListener('input', updatePointSize);
+
+  // --------------------------------------------------------------------
+  // ColorBy handling
+  // --------------------------------------------------------------------
+
+  const colorByOptions = [{ value: ':', label: 'Solid color' }].concat(
+    source
+      .getPointData()
+      .getArrays()
+      .map((a) => ({
+        label: `(p) ${a.getName()}`,
+        value: `PointData:${a.getName()}`
+      })),
+    source
+      .getCellData()
+      .getArrays()
+      .map((a) => ({
+        label: `(c) ${a.getName()}`,
+        value: `CellData:${a.getName()}`
+      }))
+  );
+  colorBySelector.innerHTML = colorByOptions
+    .map(
+      ({ label, value }) =>
+        `<option value="${value}" ${
+          field === value ? 'selected="selected"' : ''
+        }>${label}</option>`
+    )
+    .join('');
+
+  function updateColorBy (event) {
+    const [location, colorByArrayName] = event.target.value.split(':');
+    const interpolateScalarsBeforeMapping = location === 'PointData';
+    let colorMode = ColorMode.DEFAULT;
+    let scalarMode = ScalarMode.DEFAULT;
+    const scalarVisibility = location.length > 0;
+    if (scalarVisibility) {
+      const newArray = source[`get${location}`]().getArrayByName(
+        colorByArrayName
+      );
+      activeArray = newArray;
+      const newDataRange = activeArray.getRange();
+      dataRange[0] = newDataRange[0];
+      dataRange[1] = newDataRange[1];
+      colorMode = ColorMode.MAP_SCALARS;
+      scalarMode =
+        location === 'PointData'
+          ? ScalarMode.USE_POINT_FIELD_DATA
+          : ScalarMode.USE_CELL_FIELD_DATA;
+
+      const numberOfComponents = activeArray.getNumberOfComponents();
+      if (numberOfComponents > 1) {
+        // always start on magnitude setting
+        if (mapper.getLookupTable()) {
+          const lut = mapper.getLookupTable();
+          lut.setVectorModeToMagnitude();
+        }
+        componentSelector.style.display = 'block';
+        const compOpts = ['Magnitude'];
+        while (compOpts.length <= numberOfComponents) {
+          compOpts.push(`Component ${compOpts.length}`);
+        }
+        componentSelector.innerHTML = compOpts
+          .map((t, index) => `<option value="${index - 1}">${t}</option>`)
+          .join('');
+        if (numberOfComponents > 2 && numberOfComponents < 5) {
+          componentSelector.innerHTML += '<option value="-2">RGB(A)</option>';
+        }
+      } else {
+        componentSelector.style.display = 'none';
+      }
+      scalarBarActor.setAxisLabel(colorByArrayName);
+      scalarBarActor.setVisibility(true);
+    } else {
+      componentSelector.style.display = 'none';
+      scalarBarActor.setVisibility(false);
+    }
+    mapper.set({
+      colorByArrayName,
+      colorMode,
+      interpolateScalarsBeforeMapping,
+      scalarMode,
+      scalarVisibility
+    });
+    applyPreset();
+  }
+  colorBySelector.addEventListener('change', updateColorBy);
+  updateColorBy({ target: colorBySelector });
+
+  function updateColorByComponent (event) {
+    if (mapper.getLookupTable()) {
+      const lut = mapper.getLookupTable();
+      const v = Number(event.target.value);
+      if (v === -1) {
+        lut.setVectorModeToMagnitude();
+        mapper.setColorModeToMapScalars();
+        scalarBarActor.setVisibility(true);
+      } else if (v === -2) {
+        lut.setVectorModeToRGBColors();
+        mapper.setColorModeToDirectScalars();
+        scalarBarActor.setVisibility(false);
+      } else {
+        lut.setVectorModeToComponent();
+        mapper.setColorModeToMapScalars();
+        scalarBarActor.setVisibility(true);
+        lut.setVectorComponent(v);
+        const newDataRange = activeArray.getRange(v);
+        dataRange[0] = newDataRange[0];
+        dataRange[1] = newDataRange[1];
+        lookupTable.setMappingRange(dataRange[0], dataRange[1]);
+        lut.updateRange();
+      }
+      renderWindow.render();
+    }
+  }
+  componentSelector.addEventListener('change', updateColorByComponent);
+
+  // --------------------------------------------------------------------
+  // Pipeline handling
+  // --------------------------------------------------------------------
+
+  actor.setMapper(mapper);
+  mapper.setInputData(source);
+  renderer.addActor(actor);
+
+  scalarBarActor.setScalarsToColors(mapper.getLookupTable());
+
+  // Manage update when lookupTable change
+  const debouncedRender = debounce(renderWindow.render, 10);
+  lookupTable.onModified(debouncedRender, -1);
+
+  // First render
+  renderer.resetCamera();
+  renderWindow.render();
+
+  // global.pipeline[fileName] = {
+  //   actor,
+  //   mapper,
+  //   source,
+  //   lookupTable,
+  //   renderer,
+  //   renderWindow,
+  // };
+
+  // Update stats
+  fpsMonitor.update();
+}
+
+global.createPipeline = createPipeline;
+// ----------------------------------------------------------------------------
