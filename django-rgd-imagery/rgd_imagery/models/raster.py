5e528a8f80cc6292b7fcf84579eb1221b126716e
Matt Covalt <mcovalt@mailbox.org>
2021-07-13 11:07:43 -0400

    Make RangeField a DecimalRangeField

--- django-rgd-imagery/rgd_imagery/models/raster.py
+++ django-rgd-imagery/rgd_imagery/models/raster.py
@@ -61,7 +61,7 @@ class RasterMeta(TimeStampedModel, SpatialEntry, PermissionPathMixin, DetailView
     cloud_cover = models.FloatField(
         null=True, validators=[MinValueValidator(0), MaxValueValidator(100)], blank=True
     )
-    band_range = fields.RangeField(null=True)
+    band_range = fields.DecimalRangeField(null=True)
 
     @property
     def name(self):
