a90be9af2ad4a3b5c7240b37d06b478195cdd12c
Matt Covalt <mcovalt@mailbox.org>
2021-07-29 20:45:19 -0400

    Ingest/output STAC items with EO attributes (#471)
    * Add bidict of STAC common names
    
    * Output EO band min/max
    
    * Ingest EO band min/max
    
    * Remove band_num iterator
    
    The placeholder that made this iterator necessary has been removed in
    the previous commit.
    
    * Comment on how we convert center_wavelength and full_width_half_max
    
    * Undo removal of old behavior for the case of one band
    
    * Add note of assumption made regarding data shape
    
    * Fix unused variable
    
    * Fix extra blank line
    
    * Add 'band_range' to Raster.meta
    
    * Make RangeField a DecimalRangeField
    
    * Add help_text to describe what the new field means
    
    * Specify unit of measurement on band_range
    
    * Make migration
    
    * Update pystac extensions class name to new version
    
    * Use 'BandMeta.band_range' instead of min/max
    
    Using Decimal was done due to switching to Decimal in the Django model,
    but it seems like a good idea when looking up common_name too.
    
    * Move 'band_range' from 'RasterMeta' -> 'BandMeta'
    
    * Clarify 'eo_band' parser branch
    
    * Rename parser tokens to match serializer variable names
    
    * More renames of parser tokens
    
    * Reorder imports
    
    * Update API call in pystac
    
    * Move asset declaration closer to where it is operated on
    
    * Remove redundant 'single_asset' var
    
    * Reorder add_asset to make more sense
    
    * Only iterate through bands
    
    * tmp
    
    * Make note of a potentially unnormalized form
    
    * Add rounding context to decimal math
    
    * Add blank string values during model creation
    
    * Add tests
    
    * Pin PySTAC version for #477
    
    * Pin pystac to non-RC version
    
    * Prevent duplicate records being created
    
    * Patch BandMeta update in testing fixture
    
    * Linting
    
    Co-authored-by: banesullivan <bane.sullivan@kitware.com>

--- django-rgd-imagery/rgd_imagery/serializers/stac.py
+++ django-rgd-imagery/rgd_imagery/serializers/stac.py
@@ -1,5 +1,8 @@
+import decimal
+from decimal import Decimal
 import json
 
+from bidict import bidict
 import dateutil.parser
 from django.contrib.gis.geos import Polygon
 from django.db import transaction
@@ -10,6 +13,27 @@ from rgd.models import ChecksumFile, FileSourceType
 from rgd.utility import get_or_create_no_commit
 
 from .. import models
+
+BAND_RANGE_BY_COMMON_NAMES = bidict(
+    {
+        'coastal': (Decimal(0.40), Decimal(0.45)),
+        'blue': (Decimal(0.45), Decimal(0.50)),
+        'green': (Decimal(0.50), Decimal(0.60)),
+        'red': (Decimal(0.60), Decimal(0.70)),
+        'yellow': (Decimal(0.58), Decimal(0.62)),
+        'pan': (Decimal(0.50), Decimal(0.70)),
+        'rededge': (Decimal(0.70), Decimal(0.79)),
+        'nir': (Decimal(0.75), Decimal(1.00)),
+        'nir08': (Decimal(0.75), Decimal(0.90)),
+        'nir09': (Decimal(0.85), Decimal(1.05)),
+        'cirrus': (Decimal(1.35), Decimal(1.40)),
+        'swir16': (Decimal(1.55), Decimal(1.75)),
+        'swir22': (Decimal(2.10), Decimal(2.30)),
+        'lwir': (Decimal(10.5), Decimal(12.5)),
+        'lwir11': (Decimal(10.5), Decimal(11.5)),
+        'lwir12': (Decimal(11.5), Decimal(12.5)),
+    }
+)
 
 
 class STACRasterSerializer(serializers.BaseSerializer):
@@ -41,11 +65,38 @@ class STACRasterSerializer(serializers.BaseSerializer):
         item.ext.enable('eo')
         item.ext.eo.apply(cloud_cover=instance.cloud_cover, bands=[])
         # Add assets
-        band_num = 0
         for image in instance.parent_raster.image_set.images.all():
             if image.file.type != FileSourceType.URL:
                 # TODO: we need fix this
                 raise ValueError('Files must point to valid URL resources, not internal storage.')
+            bands = []
+            for bandmeta in image.bandmeta_set.filter(band_range__contained_by=(None, None)):
+                band = pystac.extensions.eo.Band.create(
+                    name=f'B{bandmeta.band_number}',
+                    description=bandmeta.description,
+                )
+                # The wavelength statistics is described by either the
+                # common_name or via center_wavelength and full_width_half_max.
+                # We can derive our bandmeta.band_range.lower,
+                # bandmeta.band_range.upper from the center_wavelength
+                # and full_width_half_max.
+                if (
+                    bandmeta.band_range.lower,
+                    bandmeta.band_range.upper,
+                ) in BAND_RANGE_BY_COMMON_NAMES.inverse:
+                    band.common_name = BAND_RANGE_BY_COMMON_NAMES.inverse[
+                        (bandmeta.band_range.lower, bandmeta.band_range.upper)
+                    ]
+                else:
+                    with decimal.localcontext(decimal.BasicContext):
+                        band.center_wavelength = float(
+                            (bandmeta.band_range.lower + bandmeta.band_range.upper) / 2
+                        )
+                        band.full_width_half_max = float(
+                            bandmeta.band_range.upper - bandmeta.band_range.lower
+                        )
+
+                bands.append(band)
             asset = pystac.Asset(
                 href=image.file.get_url(),
                 title=image.file.name,
@@ -53,27 +104,17 @@ class STACRasterSerializer(serializers.BaseSerializer):
                     'data',
                 ],
             )
-            if image.imagemeta.number_of_bands == 1:
-                bands = [
+            item.add_asset(f'image-{image.pk}', asset)
+            item.ext.eo.set_bands(
+                bands=bands
+                or [
                     pystac.extensions.eo.Band.create(
                         name=image.file.name,
                         description=image.bandmeta_set.first().description,
                     )
-                ]
-            else:
-                bands = [
-                    pystac.extensions.eo.Band.create(
-                        name=f'B{bandmeta.band_number + band_num}',
-                        description=bandmeta.description,
-                    )
-                    for bandmeta in image.bandmeta_set.all()
-                ]
-            item.ext.eo.set_bands(
-                bands=bands,
+                ],
                 asset=asset,
             )
-            item.add_asset(f'image-{image.pk}', asset)
-            band_num += image.imagemeta.number_of_bands
 
         for ancillary_file in instance.parent_raster.ancillary_files.all():
             asset = pystac.Asset(
@@ -91,24 +132,45 @@ class STACRasterSerializer(serializers.BaseSerializer):
     def create(self, data):
         item = pystac.Item.from_dict(data)
         image_ids, ancillary = [], []
-        single_asset = False
-        if len(item.assets) == 1:
-            single_asset = True
         for name in item.assets:
             asset = item.assets[name]
             checksum_file, _ = ChecksumFile.objects.get_or_create(
                 type=FileSourceType.URL,
                 url=asset.href,
             )
-            if single_asset or (asset.roles and 'data' in asset.roles):
+            if len(item.assets) == 1 or (asset.roles and 'data' in asset.roles):
                 image, _ = models.Image.objects.get_or_create(file=checksum_file)
                 image_ids.append(image.pk)
+                for eo_band in item.ext.eo.bands:
+                    if eo_band.name.startswith('B') and eo_band.name[1:].isdigit():
+                        eo_band_number = int(eo_band.name[1:])
+                    else:
+                        eo_band_number = 0  # TODO: confirm reasonable default here
+                    if eo_band.common_name in BAND_RANGE_BY_COMMON_NAMES:
+                        eo_band_spectral_lower, eo_band_spectral_upper = BAND_RANGE_BY_COMMON_NAMES[
+                            eo_band.common_name
+                        ]
+                    elif eo_band.center_wavelength and eo_band.full_width_half_max:
+                        eo_band_spectral_lower = (
+                            eo_band.eo_band_spectral_upper - eo_band.full_width_half_max / 2
+                        )
+                        eo_band_spectral_upper = (
+                            eo_band.center_wavelength + eo_band.full_width_half_max / 2
+                        )
+                    bandmeta = models.BandMeta.objects.get_or_create(
+                        parent_image=image,
+                        band_number=eo_band_number,
+                    )
+                    bandmeta.description = eo_band.description
+                    bandmeta.band_range = (
+                        Decimal(eo_band_spectral_lower),
+                        Decimal(eo_band_spectral_upper),
+                    )
+                    bandmeta.save()
             else:
                 ancillary.append(checksum_file)
 
-        image_set, image_set_created = models.get_or_create_image_set(
-            image_ids, defaults=dict(name=item.id)
-        )
+        image_set, _ = models.get_or_create_image_set(image_ids, defaults=dict(name=item.id))
 
         raster, raster_created = get_or_create_no_commit(
             models.Raster, image_set=image_set, defaults=dict(name=item.id)
