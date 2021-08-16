dc91216f8858cef9908ab012a960a507e3a62a6c
banesullivan <bane.sullivan@kitware.com>
2021-08-16 12:11:22 -0600

    linting

--- django-rgd-imagery/rgd_imagery/migrations/0004_auto_20210816_1309.py
+++ django-rgd-imagery/rgd_imagery/migrations/0004_auto_20210816_1309.py
@@ -17,12 +17,47 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='ProcessedImage',
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                (
+                    'id',
+                    models.AutoField(
+                        auto_created=True, primary_key=True, serialize=False, verbose_name='ID'
+                    ),
+                ),
                 ('failure_reason', models.TextField(null=True)),
-                ('status', models.CharField(choices=[('created', 'Created but not queued'), ('queued', 'Queued for processing'), ('running', 'Processing'), ('failed', 'Failed'), ('success', 'Succeeded')], default='created', max_length=20)),
-                ('created', django_extensions.db.fields.CreationDateTimeField(auto_now_add=True, verbose_name='created')),
-                ('modified', django_extensions.db.fields.ModificationDateTimeField(auto_now=True, verbose_name='modified')),
-                ('ancillary_files', models.ManyToManyField(blank=True, related_name='_rgd_imagery_processedimage_ancillary_files_+', to='rgd.ChecksumFile')),
+                (
+                    'status',
+                    models.CharField(
+                        choices=[
+                            ('created', 'Created but not queued'),
+                            ('queued', 'Queued for processing'),
+                            ('running', 'Processing'),
+                            ('failed', 'Failed'),
+                            ('success', 'Succeeded'),
+                        ],
+                        default='created',
+                        max_length=20,
+                    ),
+                ),
+                (
+                    'created',
+                    django_extensions.db.fields.CreationDateTimeField(
+                        auto_now_add=True, verbose_name='created'
+                    ),
+                ),
+                (
+                    'modified',
+                    django_extensions.db.fields.ModificationDateTimeField(
+                        auto_now=True, verbose_name='modified'
+                    ),
+                ),
+                (
+                    'ancillary_files',
+                    models.ManyToManyField(
+                        blank=True,
+                        related_name='_rgd_imagery_processedimage_ancillary_files_+',
+                        to='rgd.ChecksumFile',
+                    ),
+                ),
             ],
             options={
                 'get_latest_by': 'modified',
@@ -33,10 +68,37 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='ProcessedImageGroup',
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('created', django_extensions.db.fields.CreationDateTimeField(auto_now_add=True, verbose_name='created')),
-                ('modified', django_extensions.db.fields.ModificationDateTimeField(auto_now=True, verbose_name='modified')),
-                ('process_type', models.CharField(choices=[('arbitrary', 'Arbitrarily processed externally'), ('cog', 'Converted to Cloud Optimized GeoTIFF'), ('region', 'Extract subregion'), ('resample', 'Resample by factor')], default='arbitrary', max_length=20)),
+                (
+                    'id',
+                    models.AutoField(
+                        auto_created=True, primary_key=True, serialize=False, verbose_name='ID'
+                    ),
+                ),
+                (
+                    'created',
+                    django_extensions.db.fields.CreationDateTimeField(
+                        auto_now_add=True, verbose_name='created'
+                    ),
+                ),
+                (
+                    'modified',
+                    django_extensions.db.fields.ModificationDateTimeField(
+                        auto_now=True, verbose_name='modified'
+                    ),
+                ),
+                (
+                    'process_type',
+                    models.CharField(
+                        choices=[
+                            ('arbitrary', 'Arbitrarily processed externally'),
+                            ('cog', 'Converted to Cloud Optimized GeoTIFF'),
+                            ('region', 'Extract subregion'),
+                            ('resample', 'Resample by factor'),
+                        ],
+                        default='arbitrary',
+                        max_length=20,
+                    ),
+                ),
                 ('parameters', models.JSONField(blank=True, null=True)),
             ],
             options={
@@ -55,7 +117,11 @@ class Migration(migrations.Migration):
         migrations.AlterField(
             model_name='raster',
             name='ancillary_files',
-            field=models.ManyToManyField(blank=True, related_name='_rgd_imagery_raster_ancillary_files_+', to='rgd.ChecksumFile'),
+            field=models.ManyToManyField(
+                blank=True,
+                related_name='_rgd_imagery_raster_ancillary_files_+',
+                to='rgd.ChecksumFile',
+            ),
         ),
         migrations.DeleteModel(
             name='ConvertedImage',
@@ -66,16 +132,26 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='processedimage',
             name='group',
-            field=models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='rgd_imagery.processedimagegroup'),
+            field=models.ForeignKey(
+                on_delete=django.db.models.deletion.CASCADE, to='rgd_imagery.processedimagegroup'
+            ),
         ),
         migrations.AddField(
             model_name='processedimage',
             name='processed_image',
-            field=models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.SET_NULL, related_name='+', to='rgd_imagery.image'),
+            field=models.ForeignKey(
+                blank=True,
+                null=True,
+                on_delete=django.db.models.deletion.SET_NULL,
+                related_name='+',
+                to='rgd_imagery.image',
+            ),
         ),
         migrations.AddField(
             model_name='processedimage',
             name='source_images',
-            field=models.ManyToManyField(related_name='_rgd_imagery_processedimage_source_images_+', to='rgd_imagery.Image'),
+            field=models.ManyToManyField(
+                related_name='_rgd_imagery_processedimage_source_images_+', to='rgd_imagery.Image'
+            ),
         ),
     ]
