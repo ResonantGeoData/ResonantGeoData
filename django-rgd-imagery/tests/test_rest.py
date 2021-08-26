dc2ea0f738af2bdef90d6adb007c217224dcf5ed
Jacob Nesbitt <jjnesbitt2@gmail.com>
2021-08-26 16:31:57 -0400

    Remove url prefixes for rgd and rgd_imagery

--- django-rgd-imagery/tests/test_rest.py
+++ django-rgd-imagery/tests/test_rest.py
@@ -6,7 +6,7 @@ from rgd_imagery import models
 @pytest.mark.django_db(transaction=True)
 def test_download_image_file(admin_api_client, astro_image):
     pk = astro_image.pk
-    response = admin_api_client.get(f'/rgd_imagery_test/api/rgd_imagery/{pk}/data')
+    response = admin_api_client.get(f'/api/rgd_imagery/{pk}/data')
     assert status.is_redirect(response.status_code)
 
 
@@ -23,9 +23,7 @@ def test_create_get_subsampled_image(admin_api_client, astro_image):
             'bottom': 0,
         },
     }
-    response = admin_api_client.post(
-        '/rgd_imagery_test/api/image_process/group', payload, format='json'
-    )
+    response = admin_api_client.post('/api/image_process/group', payload, format='json')
     assert response.status_code == 201
     assert response.data
     group_id = response.data['id']
@@ -35,18 +33,18 @@ def test_create_get_subsampled_image(admin_api_client, astro_image):
         ],
         'group': group_id,
     }
-    response = admin_api_client.post('/rgd_imagery_test/api/image_process', payload, format='json')
+    response = admin_api_client.post('/api/image_process', payload, format='json')
     assert response.status_code == 201
     assert response.data
     id = response.data['id']
     sub = models.ProcessedImage.objects.get(id=id)
     assert sub.processed_image
     # Test the GET
-    response = admin_api_client.get(f'/rgd_imagery_test/api/image_process/{id}')
+    response = admin_api_client.get(f'/api/image_process/{id}')
     assert response.status_code == 200
     assert response.data
     # Now test to make sure the serializer prevents duplicates
-    response = admin_api_client.post('/rgd_imagery_test/api/image_process', payload, format='json')
+    response = admin_api_client.post('/api/image_process', payload, format='json')
     assert response.status_code == 201
     assert response.data
     assert id == response.data['id']  # Compare against original PK
@@ -56,7 +54,7 @@ def test_create_get_subsampled_image(admin_api_client, astro_image):
 @pytest.mark.django_db(transaction=True)
 def test_create_and_download_cog(admin_api_client, geotiff_image_entry):
     response = admin_api_client.post(
-        '/rgd_imagery_test/api/image_process/group',
+        '/api/image_process/group',
         {
             'process_type': 'cog',
         },
@@ -66,7 +64,7 @@ def test_create_and_download_cog(admin_api_client, geotiff_image_entry):
     assert response.data
     group_id = response.data['id']
     response = admin_api_client.post(
-        '/rgd_imagery_test/api/image_process',
+        '/api/image_process',
         {
             'source_images': [
                 geotiff_image_entry.id,
@@ -83,5 +81,5 @@ def test_create_and_download_cog(admin_api_client, geotiff_image_entry):
     assert cog.processed_image
     # Also test download endpoint here:
     pk = cog.pk
-    response = admin_api_client.get(f'/rgd_imagery_test/api/image_process/{pk}')
+    response = admin_api_client.get(f'/api/image_process/{pk}')
     assert response.data
