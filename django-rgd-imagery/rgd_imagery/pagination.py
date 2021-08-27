e5e1c155b12d5c60901075a9d48f77f3dc5971fa
Matt Covalt <mcovalt@mailbox.org>
2021-08-26 22:02:52 -0400

    Fix pagination

--- django-rgd-imagery/rgd_imagery/pagination.py
+++ django-rgd-imagery/rgd_imagery/pagination.py
@@ -6,5 +6,7 @@ class STACPagination(pagination.PageNumberPagination):
     page_size_query_param = 'limit'
 
     def get_paginated_response(self, data):
+        if 'links' not in data:
+            data['links'] = []
         data['links'].append({'rel': 'next', 'href': self.get_next_link()})
         return response.Response(data)
