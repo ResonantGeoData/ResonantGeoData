839581a77ec5b9e4e0a5614e5e146564010641c2
Matt Covalt <mcovalt@mailbox.org>
2021-08-26 22:44:21 -0400

    Remove null next page link

--- django-rgd-imagery/rgd_imagery/pagination.py
+++ django-rgd-imagery/rgd_imagery/pagination.py
@@ -6,6 +6,9 @@ class STACPagination(pagination.PageNumberPagination):
     page_size_query_param = 'limit'
 
     def get_paginated_response(self, data):
+        next_link = self.get_next_link()
+        if not next_link:
+            return response.Response(data)
         if 'links' not in data:
             data['links'] = []
         data['links'].append({'rel': 'next', 'href': self.get_next_link()})
