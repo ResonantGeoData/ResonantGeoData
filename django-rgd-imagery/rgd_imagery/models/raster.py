13e013e912deee3fa454198cdeaed2792810d690
banesullivan <bane.sullivan@kitware.com>
2021-09-12 21:28:56 -0600

    linting

--- django-rgd-imagery/rgd_imagery/models/raster.py
+++ django-rgd-imagery/rgd_imagery/models/raster.py
@@ -1,7 +1,6 @@
 from django.contrib.gis.db import models
 from django.contrib.postgres import fields
 from django.core.validators import MaxValueValidator, MinValueValidator
-from django.db.models import Sum
 from django_extensions.db.models import TimeStampedModel
 from rgd.models import ChecksumFile, SpatialEntry
 from rgd.models.mixins import DetailViewMixin, PermissionPathMixin, TaskEventMixin
