a91a54ca92b7dfbf1ddeb39df8f67b146aadd708
banesullivan <bane.sullivan@kitware.com>
2021-09-16 08:26:47 -0600

    Fix display name for images in raster detail view

--- django-rgd/rgd/models/common.py
+++ django-rgd/rgd/models/common.py
@@ -140,6 +140,10 @@ class ChecksumFile(TimeStampedModel, TaskEventMixin, PermissionPathMixin):
             )
         ]
 
+    @property
+    def basename(self):
+        return os.path.basename(self.name)
+
     def get_checksum(self):
         """Compute a new checksum without saving it."""
         if self.type == FileSourceType.FILE_FIELD:
