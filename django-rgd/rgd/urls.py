bbb3b167c1a7ea9a189bdb32dda7902149b82393
Mike VanDenburgh <michael.vandenburgh@kitware.com>
2021-09-30 11:39:42 -0400

    Change api token endpoint url

--- django-rgd/rgd/urls.py
+++ django-rgd/rgd/urls.py
@@ -4,6 +4,8 @@ from rest_framework.authtoken.views import obtain_auth_token
 from . import rest, views
 
 urlpatterns = [
+    # API Key Authentication
+    path('api/api-token-auth', obtain_auth_token, name='api-token-auth'),
     # Pages
     path('rgd', views.SpatialEntriesListView.as_view(), name='rgd-index'),
     path(
@@ -81,5 +83,4 @@ urlpatterns = [
         rest.get.GetSpatialAsset.as_view(),
         name='spatial-asset',
     ),
-    path('api/rgd/api-token-auth', obtain_auth_token, name='api-token-auth'),
 ]
