b2eea5453356d5e7b386a1b78e163f1f95054469
Matt Covalt <matt@kiwiziti.com>
2021-10-12 16:42:32 -0400

    Rework views to ensure permission checking

--- django-rgd-imagery/rgd_imagery/views.py
+++ django-rgd-imagery/rgd_imagery/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic.base import TemplateView
 from rgd.views import PermissionDetailView, SpatialEntriesListView, _SpatialDetailView
 
@@ -10,14 +11,16 @@ class ImageSetDetailView(PermissionDetailView):
 
 class RasterMetaEntriesListView(SpatialEntriesListView):
     model = models.RasterMeta
-    filter = filters.RasterMetaFilter
     context_object_name = 'spatial_entries'
     template_name = 'rgd_imagery/rastermeta_list.html'
+
+    def get_filter_class(self):
+        return filters.RasterMetaFilter
 
 
 class RasterDetailView(_SpatialDetailView):
     model = models.RasterMeta
 
 
-class STACBrowserView(TemplateView):
+class STACBrowserView(LoginRequiredMixin, TemplateView):
     template_name = 'rgd_imagery/stac_browser.html'
--- django-rgd/rgd/views.py
+++ django-rgd/rgd/views.py
@@ -1,6 +1,7 @@
 import json
 
 from django.contrib import messages
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.gis.db.models import Collect, Extent
 from django.contrib.gis.db.models.functions import AsGeoJSON, Centroid
 from django.contrib.postgres.aggregates import ArrayAgg
@@ -14,11 +15,17 @@ from rgd import permissions
 from . import filters, models
 
 
-class PermissionDetailView(DetailView):
+class PermissionDetailView(LoginRequiredMixin, DetailView):
     def get_object(self):
         obj = super().get_object()
         permissions.check_read_perm(self.request.user, obj)
         return obj
+
+
+class PermissionListView(LoginRequiredMixin, generic.ListView):
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        return permissions.filter_read_perm(self.request.user, queryset)
 
 
 def query_params(params):
@@ -30,12 +37,12 @@ def query_params(params):
     return '&' + query.urlencode() if query.urlencode() else ''
 
 
-class _SpatialListView(generic.ListView):
+class _SpatialListView(PermissionListView):
     paginate_by = 15
 
     def _get_extent_summary(self, object_list):
         ids = [o.spatial_id for o in object_list]
-        queryset = self.model.objects.filter(spatial_id__in=ids)
+        queryset = self.get_queryset().filter(spatial_id__in=ids)
         summary = queryset.aggregate(
             Collect('outline'),
             Extent('outline'),
@@ -76,13 +83,15 @@ class _SpatialListView(generic.ListView):
 
 class SpatialEntriesListView(_SpatialListView):
     model = models.SpatialEntry
-    filter = filters.SpatialEntryFilter
     context_object_name = 'spatial_entries'
     template_name = 'rgd/spatialentry_list.html'
 
+    def get_filter_class(self):
+        return filters.SpatialEntryFilter
+
     def get_queryset(self):
-        filterset = self.filter(data=self.request.GET)
-        queryset = self.model.objects.select_subclasses().order_by('spatial_id')
+        filterset = self.get_filterclass()
+        queryset = super().get_queryset().select_subclasses().order_by('spatial_id')
         if not filterset.is_valid():
             message = 'Filter parameters illformed. Full results returned.'
             all_error_messages_content = [
@@ -94,18 +103,14 @@ class SpatialEntriesListView(_SpatialListView):
                 messages.add_message(self.request, messages.ERROR, message)
             return queryset
         queryset = filterset.filter_queryset(queryset)
-        return permissions.filter_read_perm(self.request.user, queryset)
+        return queryset
 
 
-class StatisticsView(generic.ListView):
+class StatisticsView(PermissionListView):
     paginate_by = None
     model = models.SpatialEntry
     context_object_name = 'spatial_entries'
     template_name = 'rgd/statistics.html'
-
-    def get_queryset(self):
-        queryset = self.model.objects.all()
-        return permissions.filter_read_perm(self.request.user, queryset)
 
     def get_context_data(self, *args, **kwargs):
         context = super().get_context_data(*args, **kwargs)
@@ -141,11 +146,6 @@ class StatisticsView(generic.ListView):
 
 
 class _SpatialDetailView(PermissionDetailView):
-    def get_object(self):
-        obj = super().get_object()
-        permissions.check_read_perm(self.request.user, obj)
-        return obj
-
     def _get_extent(self, object):
         extent = {
             'count': 0,
