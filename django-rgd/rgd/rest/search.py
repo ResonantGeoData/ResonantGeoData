a75384dca05a98838b6e0b3088ce46aefbf9dd7d
Matt Covalt <matt@kiwiziti.com>
2021-10-18 18:12:52 -0400

    Lint

--- django-rgd/rgd/rest/search.py
+++ django-rgd/rgd/rest/search.py
@@ -1,4 +1,3 @@
-from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.generics import ListAPIView
 from rgd import serializers
 from rgd.filters import SpatialEntryFilter
