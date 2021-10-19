669ebbeea5eb81f16ae822074c9e4b858d28c960
Matt Covalt <matt@kiwiziti.com>
2021-10-19 17:57:23 -0400

    Add TaskEventViewMixin

--- django-rgd/rgd/viewsets.py
+++ django-rgd/rgd/viewsets.py
@@ -1,9 +1,37 @@
 from django_filters.rest_framework import DjangoFilterBackend
+from drf_yasg.utils import swagger_auto_schema
 from rest_framework import mixins, viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 
 class BaseViewMixin:
     filter_backends = [DjangoFilterBackend]
+
+
+class TaskEventViewMixin:
+    """
+    Mixin for any TaskEventMixin model.
+
+    Provides the `status` action to retrieve the status of a
+    model's task event.
+
+    TODO: This should use a serializer instead.
+    """
+
+    @swagger_auto_schema(
+        method='GET',
+        operation_summary='Check the status.',
+    )
+    @action(detail=True)
+    def status(self, *args, **kwargs):
+        obj = self.get_object()
+        data = {
+            'pk': obj.pk,
+            'model': obj.__name__,
+            'status': obj.status,
+        }
+        return Response(data)
 
 
 class ReadOnlyModelViewSet(
