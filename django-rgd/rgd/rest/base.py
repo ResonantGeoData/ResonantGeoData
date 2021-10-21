1a90a0cbd6b2401d33dca965767a7ee47327a689
Matt Covalt <matt@kiwiziti.com>
2021-10-21 16:47:52 -0400

    Specify that this mixin is for viewsets

--- django-rgd/rgd/rest/base.py
+++ django-rgd/rgd/rest/base.py
@@ -5,7 +5,7 @@ from rest_framework.response import Response
 from rgd.rest.mixins import BaseRestViewMixin
 
 
-class TaskEventViewMixin:
+class TaskEventViewSetMixin:
     """
     Mixin for any TaskEventMixin model.
 
