8849b1bda108cbd9c17845685b6f22b2d2c11d6a
Bane Sullivan <bane.sullivan@kitware.com>
2021-11-01 14:44:23 -0600

    Patch issues with STAC raster serializer (#549)
    * Patch issues with STAC raster serializer
    
    * Fix STAC Item serializer save with no spectral info
    
    * Fix issue with null eobands
    
    * Add STAC serializer test for new data
    
    Co-authored-by: Matt Covalt <matt@kiwiziti.com>

--- django-rgd-imagery/rgd_imagery/serializers/stac/item/__init__.py
+++ django-rgd-imagery/rgd_imagery/serializers/stac/item/__init__.py
@@ -19,6 +19,9 @@ from ..utils import non_unique_get_or_create
 
 
 class ItemSerializer(serializers.BaseSerializer):
+    def to_internal_value(self, data):
+        return data
+
     def _add_image_to_item(self, item, image, asset_type='image', roles=('data',)):
         asset = pystac.Asset(
             href=image.file.get_url(),
@@ -123,9 +126,10 @@ class ItemSerializer(serializers.BaseSerializer):
             if len(item.assets) == 1 or (asset.roles and 'data' in asset.roles):
                 image = non_unique_get_or_create(Image, file=checksum_file)
                 image_ids.append(image.pk)
-                for eo_band in item_eo_ext.bands:
-                    bandmeta = band_utils.to_model(eo_band, image)
-                    bandmeta.save()
+                if item_eo_ext.bands:
+                    for eo_band in item_eo_ext.bands:
+                        bandmeta = band_utils.to_model(eo_band, image)
+                        bandmeta.save()
             else:
                 ancillary.append(checksum_file)
 
