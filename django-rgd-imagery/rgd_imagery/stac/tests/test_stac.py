379b61a03d5968e7a250aa7535c235dace1865e3
Matt Covalt <matt@kiwiziti.com>
2021-11-10 13:30:50 -0500

    Remove STAC creation methods

--- django-rgd-imagery/rgd_imagery/stac/tests/test_stac.py
+++ django-rgd-imagery/rgd_imagery/stac/tests/test_stac.py
@@ -1,20 +1,9 @@
 import json
 
-import pystac
 import pytest
 from rgd.datastore import datastore
 from rgd_imagery import models
-from rgd_imagery.serializers.stac import ItemSerializer
-
-
-def _check_conforms_stac(data):
-    assert 'stac_version' in data
-    assert 'assets' in data
-    item = pystac.Item.from_dict(data)  # noqa
-    # TODO:
-    # errors = item.validate()
-    # if errors:
-    #     raise ValueError('ilformed STAC Item.')
+from rgd_imagery.stac.serializers import ItemSerializer
 
 
 @pytest.mark.django_db(transaction=True)
@@ -24,7 +13,6 @@ def test_raster_stac_serializer_simple(admin_api_client, sample_raster_url_singl
     assert response.status_code == 200
     data = response.data
     assert data
-    _check_conforms_stac(data)
     assets = data['assets']
     assert len(assets) == 2
 
@@ -36,7 +24,6 @@ def test_raster_stac_serializer_multi_file_bands(admin_api_client, sample_raster
     assert response.status_code == 200
     data = response.data
     assert data
-    _check_conforms_stac(data)
     assets = data['assets']
     assert len(assets) == 4
 
@@ -46,7 +33,6 @@ def test_eo_serialize(admin_api_client, sample_raster_url):
     id = sample_raster_url.pk
     response = admin_api_client.get(f'/api/rgd_imagery/raster/{id}/stac')
     data = response.data
-    _check_conforms_stac(data)
     for asset in data['assets'].values():
         if 'data' in asset.get('roles', []):
             assert 'eo:bands' in asset
@@ -56,51 +42,3 @@ def test_eo_serialize(admin_api_client, sample_raster_url):
                 assert 'common_name' in band or (
                     'center_wavelength' in band and 'full_width_half_max' in band
                 )
-
-
-@pytest.mark.django_db(transaction=True)
-def test_raster_stac_export_import(admin_api_client, sample_raster_url):
-    sample = sample_raster_url
-    data = ItemSerializer(sample).data
-    instance = ItemSerializer().create(data)
-    # Check Image/ImageMeta
-    assert (
-        instance.parent_raster.image_set.images.count()
-        == sample.parent_raster.image_set.images.count()
-    )
-    # Since testing URL files, make sure there are no duplicates
-    assert set(instance.parent_raster.image_set.images.all()) == set(
-        sample.parent_raster.image_set.images.all()
-    )
-    # Get checksum files and make sure they are actually the same (no duplicates)
-
-    # Same for ancillary files
-    assert (
-        instance.parent_raster.ancillary_files.count()
-        == sample.parent_raster.ancillary_files.count()
-    )
-    assert set(instance.parent_raster.ancillary_files.all()) == set(
-        sample.parent_raster.ancillary_files.all()
-    )
-
-    # Asset outline/footprint of rasters are the same
-    assert instance.footprint.equals(sample.footprint)
-    # assert instance.outline.equals(sample.outline)
-
-    # Check that no duplicate data were produced
-    assert instance.pk == sample.pk
-
-    # assert that bands were made
-    assert models.BandMeta.objects.count() == models.BandMeta.objects.count() > 0
-
-
-@pytest.mark.django_db(transaction=True)
-def test_raster_stac_new_files():
-    # Test `ItemSerializer.create` with files not already existing in DB
-    path = datastore.fetch('LC08_L1TP_034032_20200429_20200509_01_T1_sr_band3.json')
-    with open(path, 'r') as f:
-        data = json.load(f)
-
-    instance = ItemSerializer().create(data)
-    assert instance.pk
-    assert instance.parent_raster.image_set.images.count() == 1
