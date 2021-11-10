347f68d2a2ecaa6bf0ea3aa130c70d3179eca9c8
Matt Covalt <matt@kiwiziti.com>
2021-11-10 14:14:03 -0500

    Fully remove get/post files

--- django-rgd-imagery/rgd_imagery/rest/post.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.generics import CreateAPIView
-from rgd.rest.mixins import BaseRestViewMixin
-from rgd_imagery import models, serializers
-
-
-class CreateRasterSTAC(BaseRestViewMixin, CreateAPIView):
-    queryset = models.RasterMeta.objects.all()
-    serializer_class = serializers.stac.ItemSerializer
