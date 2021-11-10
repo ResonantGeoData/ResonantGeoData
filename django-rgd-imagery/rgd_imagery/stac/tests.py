098c2dcb477322f29183b8e5e092ef3352ed05d0
Matt Covalt <matt@kiwiziti.com>
2021-11-10 14:05:21 -0500

    Fix tests

--- django-rgd-imagery/rgd_imagery/stac/tests.py
+++ django-rgd-imagery/rgd_imagery/stac/tests.py
@@ -1,9 +1,6 @@
-import json
 import re
 
 import pytest
-from rgd.datastore import datastore
-from rgd_imagery import models
 from rgd_imagery.stac.serializers import ItemSerializer
 
 
@@ -22,32 +19,22 @@ def test_stac_browser_limit(settings, admin_api_client, sample_raster_url):
 
 
 @pytest.mark.django_db(transaction=True)
-def test_raster_stac_serializer_simple(admin_api_client, sample_raster_url_single):
-    id = sample_raster_url_single.pk
-    response = admin_api_client.get(f'/api/rgd_imagery/raster/{id}/stac')
-    assert response.status_code == 200
-    data = response.data
-    assert data
+def test_raster_stac_serializer_simple(sample_raster_url_single):
+    data = ItemSerializer().to_representation(sample_raster_url_single)
     assets = data['assets']
     assert len(assets) == 2
 
 
 @pytest.mark.django_db(transaction=True)
-def test_raster_stac_serializer_multi_file_bands(admin_api_client, sample_raster_url):
-    id = sample_raster_url.pk
-    response = admin_api_client.get(f'/api/rgd_imagery/raster/{id}/stac')
-    assert response.status_code == 200
-    data = response.data
-    assert data
+def test_raster_stac_serializer_multi_file_bands(sample_raster_url):
+    data = ItemSerializer().to_representation(sample_raster_url)
     assets = data['assets']
     assert len(assets) == 4
 
 
 @pytest.mark.django_db(transaction=True)
-def test_eo_serialize(admin_api_client, sample_raster_url):
-    id = sample_raster_url.pk
-    response = admin_api_client.get(f'/api/rgd_imagery/raster/{id}/stac')
-    data = response.data
+def test_eo_serialize(sample_raster_url):
+    data = ItemSerializer().to_representation(sample_raster_url)
     for asset in data['assets'].values():
         if 'data' in asset.get('roles', []):
             assert 'eo:bands' in asset
