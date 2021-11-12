802fd2b11e0e88f1d01cda2d355a87630b4b0219
banesullivan <bane.sullivan@kitware.com>
2021-11-12 10:29:31 -0700

    Add support for memcached with large_image

--- django-rgd-imagery/rgd_imagery/__init__.py
+++ django-rgd-imagery/rgd_imagery/__init__.py
@@ -1,7 +1,29 @@
 from pkg_resources import DistributionNotFound, get_distribution
+import logging
+
+from django.conf import settings
+import large_image
 
 try:
     __version__ = get_distribution('django-rgd-imagery').version
 except DistributionNotFound:
     # package is not installed
     __version__ = None
+
+
+logger = logging.getLogger(__name__)
+
+# Set config options for large_image
+try:
+    large_image.config.setConfig('cache_backend', 'memcached')
+    large_image.config.setConfig('cache_memcached_url', getattr(settings, 'RGD_MEMCACHED_URL'))
+    if hasattr(settings, 'RGD_MEMCACHED_USERNAME'):
+        large_image.config.setConfig(
+            'cache_memcached_username', getattr(settings, 'RGD_MEMCACHED_USERNAME')
+        )
+        large_image.config.setConfig(
+            'cache_memcached_password', getattr(settings, 'RGD_MEMCACHED_PASSWORD')
+        )
+    logger.info('Configured for memcached.')
+except AttributeError:
+    logger.info('Settings not properly configured for memcached.')
