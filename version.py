2e8636ac40cae96637c612460347a564dd51570b
banesullivan <bane.sullivan@kitware.com>
2021-11-15 13:10:50 -0700

    Release 0.2.7

--- version.py
+++ version.py
@@ -15,7 +15,7 @@ Which denotes the first release candidate.
 
 """
 # major, minor, patch
-version_info = 0, 2, '6dev10'
+version_info = 0, 2, 7
 
 # Nice string for the version
 __version__ = '.'.join(map(str, version_info))
