856d33a88ffff72307e9f7e36a90a8787399ca53
banesullivan <bane.sullivan@kitware.com>
2021-11-15 00:09:32 -0700

    Set GDAL WMS config

--- django-rgd-imagery/rgd_imagery/__init__.py
+++ django-rgd-imagery/rgd_imagery/__init__.py
@@ -5,3 +5,10 @@ try:
 except DistributionNotFound:
     # package is not installed
     __version__ = None
+
+
+from osgeo import gdal
+from rgd.utility import get_temp_dir
+
+gdal.SetConfigOption('GDAL_ENABLE_WMS_CACHE', 'YES')
+gdal.SetConfigOption('GDAL_DEFAULT_WMS_CACHE_PATH', str(get_temp_dir() / 'gdalwmscache'))
