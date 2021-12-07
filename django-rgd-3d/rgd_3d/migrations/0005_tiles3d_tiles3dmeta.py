b2ab05d07e5e46e170fbde6dbd264f581f12a53f
banesullivan <bane.sullivan@kitware.com>
2021-12-07 11:36:06 -0700

    Linting

--- django-rgd-3d/rgd_3d/migrations/0005_tiles3d_tiles3dmeta.py
+++ django-rgd-3d/rgd_3d/migrations/0005_tiles3d_tiles3dmeta.py
@@ -17,28 +17,94 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Tiles3D',
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                (
+                    'id',
+                    models.AutoField(
+                        auto_created=True, primary_key=True, serialize=False, verbose_name='ID'
+                    ),
+                ),
                 ('failure_reason', models.TextField(null=True)),
-                ('status', models.CharField(choices=[('created', 'Created but not queued'), ('queued', 'Queued for processing'), ('running', 'Processing'), ('failed', 'Failed'), ('success', 'Succeeded'), ('skipped', 'Skipped')], default='created', max_length=20)),
-                ('created', django_extensions.db.fields.CreationDateTimeField(auto_now_add=True, verbose_name='created')),
-                ('modified', django_extensions.db.fields.ModificationDateTimeField(auto_now=True, verbose_name='modified')),
+                (
+                    'status',
+                    models.CharField(
+                        choices=[
+                            ('created', 'Created but not queued'),
+                            ('queued', 'Queued for processing'),
+                            ('running', 'Processing'),
+                            ('failed', 'Failed'),
+                            ('success', 'Succeeded'),
+                            ('skipped', 'Skipped'),
+                        ],
+                        default='created',
+                        max_length=20,
+                    ),
+                ),
+                (
+                    'created',
+                    django_extensions.db.fields.CreationDateTimeField(
+                        auto_now_add=True, verbose_name='created'
+                    ),
+                ),
+                (
+                    'modified',
+                    django_extensions.db.fields.ModificationDateTimeField(
+                        auto_now=True, verbose_name='modified'
+                    ),
+                ),
                 ('name', models.CharField(blank=True, max_length=1000)),
                 ('description', models.TextField(blank=True, null=True)),
-                ('json_file', models.ForeignKey(help_text='The `tileset.json` file that contains metadata about this set of 3D tiles.', on_delete=django.db.models.deletion.CASCADE, related_name='+', to='rgd.checksumfile')),
+                (
+                    'json_file',
+                    models.ForeignKey(
+                        help_text='The `tileset.json` file that contains metadata about this set of 3D tiles.',
+                        on_delete=django.db.models.deletion.CASCADE,
+                        related_name='+',
+                        to='rgd.checksumfile',
+                    ),
+                ),
             ],
             options={
                 'verbose_name': '3D tiles',
                 'verbose_name_plural': '3D tiles',
             },
-            bases=(models.Model, rgd.models.mixins.PermissionPathMixin, rgd.models.mixins.DetailViewMixin),
+            bases=(
+                models.Model,
+                rgd.models.mixins.PermissionPathMixin,
+                rgd.models.mixins.DetailViewMixin,
+            ),
         ),
         migrations.CreateModel(
             name='Tiles3DMeta',
             fields=[
-                ('spatialentry_ptr', models.OneToOneField(auto_created=True, on_delete=django.db.models.deletion.CASCADE, parent_link=True, primary_key=True, serialize=False, to='rgd.spatialentry')),
-                ('created', django_extensions.db.fields.CreationDateTimeField(auto_now_add=True, verbose_name='created')),
-                ('modified', django_extensions.db.fields.ModificationDateTimeField(auto_now=True, verbose_name='modified')),
-                ('source', models.OneToOneField(on_delete=django.db.models.deletion.CASCADE, to='rgd_3d.tiles3d')),
+                (
+                    'spatialentry_ptr',
+                    models.OneToOneField(
+                        auto_created=True,
+                        on_delete=django.db.models.deletion.CASCADE,
+                        parent_link=True,
+                        primary_key=True,
+                        serialize=False,
+                        to='rgd.spatialentry',
+                    ),
+                ),
+                (
+                    'created',
+                    django_extensions.db.fields.CreationDateTimeField(
+                        auto_now_add=True, verbose_name='created'
+                    ),
+                ),
+                (
+                    'modified',
+                    django_extensions.db.fields.ModificationDateTimeField(
+                        auto_now=True, verbose_name='modified'
+                    ),
+                ),
+                (
+                    'source',
+                    models.OneToOneField(
+                        on_delete=django.db.models.deletion.CASCADE, to='rgd_3d.tiles3d'
+                    ),
+                ),
             ],
             options={
                 'get_latest_by': 'modified',
