1329a7885bb7e2c962c06ade76a58328b5bceca0
Matt Covalt <mcovalt@mailbox.org>
2021-12-08 14:23:43 -0500

    Fix swagger error (#626)
    * Test for swagger error
    
    * Fix lint error from accidental removal of import
    
    * Pro tip: don't commit and push breakpoints
    
    * Fix swagger error

--- django-rgd-3d/tests/conftest.py
+++ django-rgd-3d/tests/conftest.py
@@ -3,6 +3,7 @@ import inspect
 import factory
 from pytest_factoryboy import register
 from rgd_testing_utils import factories
+from rgd_testing_utils.api_fixtures import *  # noqa
 from rgd_testing_utils.data_fixtures import *  # noqa
 
 for _, fac in inspect.getmembers(factories):
