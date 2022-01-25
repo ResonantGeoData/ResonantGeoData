0270faccb0ff4e15981babe88fcdbdea4ae70a2b
banesullivan <bane.sullivan@kitware.com>
2022-01-25 12:40:27 -0700

    Use dev0

--- version.py
+++ version.py
@@ -15,7 +15,7 @@ Which denotes the first release candidate.
 
 """
 # major, minor, patch
-version_info = 0, 2, '16dev'
+version_info = 0, 2, '16dev0'
 
 # Nice string for the version
 __version__ = '.'.join(map(str, version_info))
