2e1eda0eb076d1087322777c1ffdbccc38200577
banesullivan <bane.sullivan@kitware.com>
2022-04-01 09:59:34 -0600

    Offload caching configuration

--- django-rgd-imagery/rgd_imagery/apps.py
+++ django-rgd-imagery/rgd_imagery/apps.py
@@ -1,8 +1,6 @@
 import logging
 
 from django.apps import AppConfig
-from django.conf import settings
-import large_image
 
 logger = logging.getLogger(__name__)
 
@@ -13,21 +11,3 @@ class RGDImageryConfig(AppConfig):
 
     def ready(self):
         import rgd_imagery.signals  # noqa: F401
-
-        # Set up memcached with large_image
-        if hasattr(settings, 'MEMCACHED_URL') and settings.MEMCACHED_URL:
-            large_image.config.setConfig('cache_memcached_url', settings.MEMCACHED_URL)
-            if (
-                hasattr(settings, 'MEMCACHED_USERNAME')
-                and settings.MEMCACHED_USERNAME
-                and hasattr(settings, 'MEMCACHED_PASSWORD')
-                and settings.MEMCACHED_PASSWORD
-            ):
-                large_image.config.setConfig(
-                    'cache_memcached_username', settings.MEMCACHED_USERNAME
-                )
-                large_image.config.setConfig(
-                    'cache_memcached_password', settings.MEMCACHED_PASSWORD
-                )
-            large_image.config.setConfig('cache_backend', 'memcached')
-            logger.info('large_image is configured for memcached.')
