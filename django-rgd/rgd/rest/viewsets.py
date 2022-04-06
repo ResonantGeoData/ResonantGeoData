5503ff667775d2c30a74cc341d8157a2cd775592
banesullivan <bane.sullivan@kitware.com>
2022-04-06 09:49:48 -0600

    get_object_or_404

--- django-rgd/rgd/rest/viewsets.py
+++ django-rgd/rgd/rest/viewsets.py
@@ -1,4 +1,5 @@
 from django.conf import settings
+from django.shortcuts import get_object_or_404
 from drf_yasg.utils import swagger_auto_schema
 from rest_framework import response, views
 from rest_framework.decorators import action
@@ -29,7 +30,7 @@ class CollectionViewSet(ModelViewSet):
         url_path=r'item/(?P<index>\d+)',
     )
     def item(self, request, pk, index):
-        collection = models.Collection.objects.get(pk=pk)
+        collection = get_object_or_404(models.Collection, pk=pk)
         files = collection.checksumfiles.order_by('pk')
         try:
             instance = files[int(index)]
