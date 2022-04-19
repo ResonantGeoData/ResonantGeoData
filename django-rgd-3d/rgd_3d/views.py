875123e1de216ab4fc2f334d65453635598b5484
Mike VanDenburgh <37340715+mvandenburgh@users.noreply.github.com>
2022-04-19 16:42:08 -0400

    ImageSet ListView (#741)
    * Base template for generic list views
    
    * List view for `ImageSet`
    
    * Add URL path
    
    * Add list view for Mesh3D (#742)
    
    Co-authored-by: banesullivan <bane.sullivan@kitware.com>

--- django-rgd-3d/rgd_3d/views.py
+++ django-rgd-3d/rgd_3d/views.py
@@ -1,5 +1,12 @@
-from rgd.views import PermissionDetailView
+from rgd.views import PermissionDetailView, PermissionListView
 from rgd_3d import models
+
+
+class Mesh3DListView(PermissionListView):
+    paginate_by = 15
+    context_object_name = 'mesh3ds'
+    queryset = models.Mesh3D.objects.all()
+    template_name = 'rgd_3d/mesh3d_list.html'
 
 
 class Mesh3DDetailView(PermissionDetailView):
